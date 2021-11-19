4ad6285878efb70af6d17dcdd6a1af1c2345614e
David Boyne <davidboyne@Davids-MBP.lan>
2021-11-19 12:44:57 +0000

    adding deployment code

--- packages/create-eventcatalog/create-app.ts
+++ packages/create-eventcatalog/create-app.ts
@@ -188,6 +188,7 @@ export async function createApp({
      * Default dependencies.
      */
     const dependencies = ['@eventcatalogtest/core-test']
+    
     /**
      * Default devDependencies.
      */
