de76554579699d22370b5b3b569071b34d1e6177
David Boyne <davidboyne@Davids-MBP.lan>
2022-01-06 15:07:24 +0000

    fixing bugs

--- packages/eventcatalog/pages/api/event/[name]/download.js
+++ packages/eventcatalog/pages/api/event/[name]/download.js
@@ -1,17 +1,25 @@
 import fs from 'fs';
 import path from 'path';
 
+// eslint-disable-next-line func-names
 export default function (req, res) {
   const { name: eventName } = req.query;
-  res.status(404).end();
 
-  const eventsDir = './';
+  const eventDir = path.join(process.env.PROJECT_DIR, 'events', eventName);
 
   try {
-    const schema = fs.readFileSync(path.join(eventsDir, eventName, 'schema.json'));
-    res.setHeader('Content-Type', 'application/json');
-    res.send(schema);
-    res.end();
+
+    // Find the schema file
+    const filesInEventDir = fs.readdirSync(eventDir)
+    const schemaFileName = filesInEventDir.find(fileName => fileName.indexOf('schema.') > -1);
+
+    if(schemaFileName) {
+      const schemaFile = fs.readFileSync(path.join(eventDir, schemaFileName))
+      res.send(schemaFile).end();
+    } else {
+      res.status(404).end();  
+    }
+    
   } catch (error) {
     console.log(error);
     res.status(404).end();
