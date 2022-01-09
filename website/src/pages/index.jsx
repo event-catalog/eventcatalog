ca62b80ae6e75f5be476bf3ce6d48e4e68dbb6c2
David Boyne <davidboyne@Davids-MBP.lan>
2022-01-09 19:58:36 +0000

    docs: adding opengraph tags

--- website/src/pages/index.jsx
+++ website/src/pages/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import Head from '@docusaurus/Head';
 
 import Hero from '../components/Hero';
 import Features from '../components/Features';
@@ -8,9 +9,37 @@ import CTA from '../components/CTA';
 export default function Home() {
   return (
     <Layout
-      title="EventCatalog: Simple tool to document your events"
-      description="Description will go into a meta tag in <head />"
+      title="EventCatalog: Discover, Explore and Document your Event Driven Architectures."
+      description="An open source tool powered by markdown to document your Event Driven Architecture."
     >
+      <Head>
+        <meta
+          name="description"
+          content="An open source tool powered by markdown to document your Event Driven Architecture."
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="https://twitter.com/boyney123" />
+        <meta name="twitter:creator" content="https://twitter.com/boyney123" />
+        <meta property="og:url" content="https://eventcatalog.dev/" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="EventCatalog: Discover, Explore and Document your Event Driven Architectures."
+        />
+        <meta
+          property="og:description"
+          content="An open source tool powered by markdown to document your Event Driven Architecture."
+        />
+        <meta property="og:image" content="https://demo.eventcatalog.dev/opengraph.png" />
+        <meta
+          property="og:image:alt"
+          content="EventCatalog: Discover, Explore and Document your Event Driven Architectures."
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="600" />
+        <meta property="og:locale" content="en-GB" />
+        <meta name="author" content="David Boyne" />
+      </Head>
       <main id="tailwind">
         <Hero />
         <Features />
