4a0017b7a315d1823a44d921193b88f3a2fa4e87
David Boyne <davidboyne@Davids-MBP.lan>
2022-01-10 09:52:05 +0000

    fix: edit button now working on events and services

--- packages/eventcatalog/components/ContentView.tsx
+++ packages/eventcatalog/components/ContentView.tsx
@@ -79,16 +79,19 @@ export default function ContentView({
                           )}
                         </div>
                         <div className="mt-4 flex space-x-3 md:mt-0">
-                          <button
+                          <a
+                            href={editUrl}
+                            target="_blank"
                             type="button"
                             className="hidden md:inline-flex h-10 justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                            rel="noreferrer"
                           >
                             <PencilIcon
                               className="-ml-1 mr-2 h-5 w-5 text-gray-400"
                               aria-hidden="true"
                             />
                             <span>Edit</span>
-                          </button>
+                          </a>
                         </div>
                       </div>
                       <div className="py-3 xl:pt-6 xl:pb-0">
