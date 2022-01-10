ddc92b3d42d55ef9340f90102737ecafe19ff807
David Boyne <davidboyne@Davids-MBP.lan>
2022-01-10 15:56:00 +0000

    feat: added more customise options and documentation for them

--- packages/eventcatalog/components/Footer.tsx
+++ packages/eventcatalog/components/Footer.tsx
@@ -11,9 +11,9 @@ export default function Footer() {
         {navigation && (
           <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
             {navigation.map((item) => (
-              <div key={item.name} className="px-5 py-2">
+              <div key={item.label} className="px-5 py-2">
                 <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
-                  {item.name}
+                  {item.label}
                 </a>
               </div>
             ))}
