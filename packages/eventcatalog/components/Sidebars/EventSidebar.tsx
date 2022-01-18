0be8f58e73d0b063721fc09ea84324419accccf1
Tim <>
2022-01-18 11:17:20 +0100

    Link to external event documentation

--- packages/eventcatalog/components/Sidebars/EventSidebar.tsx
+++ packages/eventcatalog/components/Sidebars/EventSidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { CubeIcon, DownloadIcon } from '@heroicons/react/outline';
+import { CubeIcon, DownloadIcon, GlobeAltIcon } from '@heroicons/react/outline';
 import type { Event } from '@eventcatalog/types';
 import fileDownload from 'js-file-download';
 import { useUser } from '@/hooks/EventCatalog';
@@ -13,7 +13,7 @@ interface EventSideBarProps {
 function EventSideBar({ event, loadedVersion }: EventSideBarProps) {
   const { getUserById } = useUser();
 
-  const { name: eventName, owners, producers, consumers, historicVersions } = event;
+  const { name: eventName, owners, producers, consumers, historicVersions, extLinks } = event;
 
   const handleDownload = async () => {
     try {
@@ -194,6 +194,18 @@ function EventSideBar({ event, loadedVersion }: EventSideBarProps) {
             </a>
           </Link>
         )}
+        {extLinks.length > 0 && extLinks.map((tag) => (
+            <a
+              href={tag.url}
+              target="_blank"
+              type="button"
+              className="hidden w-full md:inline-flex h-10 justify-center px-4 py-2 border border-teal-300 shadow-sm text-sm font-medium rounded-md text-teal-800 hover:bg-teal-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-200"
+              rel="noreferrer"
+              >
+                <GlobeAltIcon className="-ml-1 mr-2 h-5 w-5 text-teal-200" aria-hidden="true" />
+                <span>{`${tag.label}`}</span>
+              </a>
+          ))}
       </div>
     </aside>
   );
