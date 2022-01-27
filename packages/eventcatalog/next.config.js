cc8eb2fe14501447206bdb8fe141a0575c753636
Benjamin Otto <benjamin.otto@style-systems.de>
2022-01-27 13:04:20 +0100

    Allow to configure "trailingSlash" (#121)
    Co-authored-by: David Boyne <boyneyy123@gmail.com>

--- packages/eventcatalog/next.config.js
+++ packages/eventcatalog/next.config.js
@@ -4,6 +4,7 @@ const config = require('./eventcatalog.config');
 module.exports = {
   reactStrictMode: true,
   basePath: config.basePath,
+  trailingSlash: config.trailingSlash,
   publicRuntimeConfig: {
     basePath: config.basePath,
   },
