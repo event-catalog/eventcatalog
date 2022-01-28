e03169a1ae5f012b8376a6580cb8437d9f8c8f84
David Boyne <davidboyne@Davids-MBP.lan>
2022-01-28 07:34:39 +0000

    chore: stopping preview deploys with NextJS

---
+++ scripts/ignore-nextjs-preview.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "VERCEL_ENV: $VERCEL_ENV"
+
+if [[ "$VERCEL_ENV" == "production" ]] ; then
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
