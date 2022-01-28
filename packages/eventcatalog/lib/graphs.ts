588d493054a58292e65c27cffd9c31fd90c363b8
Tim <>
2022-01-28 20:17:22 +0100

    feat: mermaid name and link

--- packages/eventcatalog/lib/graphs.ts
+++ packages/eventcatalog/lib/graphs.ts
@@ -1,35 +1,82 @@
 import type { Event, Service } from '@eventcatalog/types';
+import getConfig from 'next/config';
 
 const MAX_LENGTH_FOR_NODES = '50';
+const { publicRuntimeConfig: { basePath = '' } = {} } = getConfig();
 
 const truncateNode = (value) => (value.length > MAX_LENGTH_FOR_NODES ? `${value.substring(0, MAX_LENGTH_FOR_NODES)}...` : value);
+const generateLink = (value, type) => (basePath !== '' ? `${basePath}/${type}/${value}` : `/${type}/${value}`);
 
-const buildMermaid = (centerNode, leftNodes, rightNodes, rootNodeColor) => {
-  // mermaid does not work with spaces in nodes
-  const removeSpacesInNames = (nodes) => nodes.map((node) => node.replace(/ /g, '_'));
-  const lNodes = removeSpacesInNames(leftNodes).map(truncateNode);
-  const rNodes = removeSpacesInNames(rightNodes).map(truncateNode);
-  const nodeValue = truncateNode(centerNode.replace(/ /g, '_'));
+/**
+ * Build Mermaid graph output
+ * @param centerNode
+ * @param leftNodes
+ * @param rightNodes
+ * @param rootNodeColor
+ */
+const buildMermaid = (centerNode, leftNodes, rightNodes, rootNodeColor) => `flowchart LR\n
+${leftNodes.map((node) => `${node.id}[${node.name}]:::producer-->${centerNode.id}[${centerNode.name}]:::event\n`).join('')}
+classDef event stroke:${rootNodeColor},stroke-width: 4px;\n
+classDef producer stroke:#75d7b6,stroke-width: 2px;\n
+classDef consumer stroke:#818cf8,stroke-width: 2px;\n
+${rightNodes.map((node) => `${centerNode.id}[${centerNode.name}]:::event-->${node.id}[${node.name}]:::consumer\n`).join('')}
+${leftNodes.map((node) => `click ${node.id} href "${node.link}" "Go to ${node.name}" _self\n`).join('')}
+${rightNodes.map((node) => `click ${node.id} href "${node.link}" "Go to ${node.name}" _self\n`).join('')}
+click ${centerNode.id} href "${centerNode.link}" "Go to ${centerNode.name}" _self\n
+`;
 
-  return `flowchart LR
-${lNodes.map((node) => `${node}:::producer-->${nodeValue}:::event\n`).join('')}
-classDef event stroke:${rootNodeColor},stroke-width: 4px;
-classDef producer stroke:#75d7b6,stroke-width: 2px;
-classDef consumer stroke:#818cf8,stroke-width: 2px;
-${rNodes.map((node) => `${nodeValue}:::event-->${node}:::consumer\n`).join('')}
-  `;
+/**
+ * Builds a graph for a given event
+ * @param {Event} event
+ * @param {string} rootNodeColor of the root node
+ * @returns {string} Mermaid Graph
+ */
+export const buildMermaidFlowChartForEvent = ({ name: eventName, producers, consumers }: Event, rootNodeColor = '#2563eb') => {
+  // Transforms services & event into a graph model
+  const leftNodes = producers.map(truncateNode).map((node) => ({
+    id: node.replace(/ /g, '_'),
+    name: node,
+    link: generateLink(node, 'services'),
+  }));
+  const rightNodes = consumers.map(truncateNode).map((node) => ({
+    id: node.replace(/ /g, '_'),
+    name: node,
+    link: generateLink(node, 'services'),
+  }));
+  const centerNode = {
+    id: truncateNode(eventName.replace(/ /g, '_')),
+    name: eventName,
+    link: generateLink(eventName, 'events'),
+  };
+
+  return buildMermaid(centerNode, leftNodes, rightNodes, rootNodeColor);
 };
 
-export const buildMermaidFlowChartForEvent = ({ name: eventName, producers, consumers }: Event, rootNodeColor = '#2563eb') =>
-  buildMermaid(eventName, producers, consumers, rootNodeColor);
-
+/**
+ * Builds a graph for a given service
+ * @param {Service} service
+ * @param {string} rootNodeColor of the root node
+ * @returns {string} Mermaid Graph
+ */
 export const buildMermaidFlowChartForService = (
   { publishes, subscribes, name: serviceName }: Service,
   rootNodeColor = '#2563eb'
-) =>
-  buildMermaid(
-    serviceName,
-    subscribes.map((s) => s.name),
-    publishes.map((p) => p.name),
-    rootNodeColor
-  );
+) => {
+  // Transforms services & event into a graph model
+  const leftNodes = publishes.map(truncateNode).map((node) => ({
+    id: node.name.replace(/ /g, '_'),
+    name: node.name,
+    link: generateLink(node.name, 'events'),
+  }));
+  const rightNodes = subscribes.map(truncateNode).map((node) => ({
+    id: node.name.replace(/ /g, '_'),
+    name: node.name,
+    link: generateLink(node.name, 'events'),
+  }));
+  const centerNode = {
+    id: truncateNode(serviceName.replace(/ /g, '_')),
+    name: serviceName,
+    link: generateLink(serviceName, 'services'),
+  };
+  return buildMermaid(centerNode, leftNodes, rightNodes, rootNodeColor);
+};
