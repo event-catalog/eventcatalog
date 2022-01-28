7e6abf6759ab8f0ac0d40b82ee7e5910603b1400
David Boyne <davidboyne@Davids-MBP.lan>
2022-01-28 10:53:18 +0000

    docs: updated homepage with more info

--- website/docs/api/plugins/overview.md
+++ website/docs/api/plugins/overview.md
@@ -6,25 +6,49 @@ sidebar_label: Plugins overview
 slug: '/api/plugins'
 ---
 
-We provide official EventCatalog plugins.
-
-
 ## Generator plugins {#generator-plugins}
 
-EventCatalog allows you to generate documentation/schemas and much more using it's Plugin API.
+EventCatalog allows you to generate documentation/schemas and much more using it's Plugin API. You can generate documentation from any third party system.
+
+## How Generator Plugins Work
+
+EventCatalog is built to support many brokers, languages and specifications. This allows us to remain technology agnostic and integrate with users existing solutions.
+
+EventCatalog supports `generation plugins` which you can use to generate documentation from any third party source.
+
+:::tip
+Want to integrate with something else? You can write your own plugin! You can use the `@eventcatalog/utils` to help you too!
+:::
+
+Let's take a quick look at how the generation works...
+
+![architecture](/img/blog/asyncapi-plugin/architecture.png)
+
+As we see in the diagram above, EventCatalog supports a `generation` phase. This means when you run `npm run generate` on your catalog it will go through all your [configured plugins](/docs/api/plugins/) and execute them.
+
+
+
+
+## Want to build your own plugin?
+
+It is possible to build your own plugin for any third party system you may have.
+
+**Generator Plugins** are scripts that run before the EventCatalog runs. You can use the `@eventcatalog/utils` node package to help you create your plugins (writeEvents, getEventsFromCatalog).
+
+For an example of how to write your own we recommened looking at the current plugins we have on GitHub.
+
+If you want to know more or need help we highly recommend you join us on [Discord](https://discord.gg/3rjaZMmrAm) where we can help.
+
+
+
+:::note
+We are currently working on Plugin API and Documentation.
+:::
 
 ### Generator Plugins
 
 These plugins can be used to generate documentation from third party solutions.
 
 - [@eventcatalog/plugin-doc-generator-asyncapi](plugins/@eventcatalog/plugin-doc-generator-asyncapi)
+- [@eventcatalog/plugin-doc-generator-amazon-eventbridge](plugins/@eventcatalog/plugin-doc-generator-amazon-eventbridge)
 
-:::warning
-The Plugin API is currently experimental and will change soon in future releases.
-
-We will have clear API and docs on how to contribute your own Plugins and Utility functions you can use using EventCatalog packages.
-
-You will be able to generate documentation from any third party tool you want...
-
-**This is the focus on the next release.**
-:::
--- website/src/components/Features.js
+++ website/src/components/Features.js
@@ -104,7 +104,8 @@ export default function Example() {
                   Document your events and upstream/downstream services with Markdown and our custom MDX components.
                 </p>
                 <p className="mt-3 text-lg text-gray-500">
-                  Don&apos;t want to maintain your documentation? EventCatalog supports Plugins to generate documentation for you.
+                  Don&apos;t want to maintain your documentation? EventCatalog can generate documentation from any third party
+                  system using plugins.
                 </p>
               </div>
             </div>
@@ -201,18 +202,120 @@ export default function Example() {
           </div>
 
           <hr className="hidden md:block mt-28 mb-14 border-gray-100" />
-
-          <div className="hidden md:block mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
-            <div>
-              <img className="w-16 py-8 mx-auto" src="/img/dictionary.svg" alt="Sun icon" />
-              <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">Event Ownership</p>
-              <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
-                Specify owners for each event or service. <br />
-                Quickly find who owns their part of the architecture.
-              </p>
+          <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8">
+            <div className="mt-10 -mx-4 relative lg:mt-0 z-20" aria-hidden="true">
+              <svg
+                className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
+                width={784}
+                height={404}
+                fill="none"
+                viewBox="0 0 784 404"
+              >
+                <defs>
+                  <pattern
+                    id="ca9667ae-9f92-4be7-abcb-9e3d727f2941"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                  </pattern>
+                </defs>
+                <rect width={784} height={404} fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)" />
+              </svg>
+              <img
+                id="page-example"
+                width={520}
+                className="px-4 md:px-0 z-90 mx-auto shadow-md "
+                src="/img/page-example.png"
+                alt=""
+              />
             </div>
-            <div className="mt-12">
-              <img className="rounded-lg shadow-xl ring-1 ring-black ring-opacity-5" src="/img/user-page.png" alt="" />
+            <div className="relative">
+              <img className="w-16 pt-8 pb-8" src="/img/nodes.svg" alt="Sun icon" />
+              <h3 className="text-3xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+                Generate your documentation with plugins
+              </h3>
+              <div className="space-y-6 md:space-y-10">
+                <p className="mt-3 text-lg text-gray-500">
+                  EventCatalog supports{' '}
+                  <a className="font-bold underline" href="/docs/api/plugins">
+                    generation plugins{' '}
+                  </a>
+                  which allow you to generate your documentation from any third party system.
+                </p>
+                <p className="mt-3 text-lg text-gray-500">
+                  EventCatalog currently supports documentation generation from{' '}
+                  <a
+                    className="font-bold underline"
+                    target="_blank"
+                    href="https://www.asyncapi.com/?ref=eventcatalog"
+                    rel="noreferrer"
+                  >
+                    AsyncAPI
+                  </a>{' '}
+                  and{' '}
+                  <a
+                    className="font-bold underline"
+                    target="_blank"
+                    href="https://aws.amazon.com/eventbridge?ref=eventcatalog"
+                    rel="noreferrer"
+                  >
+                    Amazon EventBridge
+                  </a>
+                  , but you can create your own plugin.
+                </p>
+                <p className="mt-3 text-lg text-gray-500">
+                  Want to contribute a new plugin or integrate with your system? Feel free to raise and issue or contribute your
+                  Plugin to EventCatalog!
+                </p>
+              </div>
+            </div>
+          </div>
+          <hr className="hidden md:block mt-28 mb-14 border-gray-100" />
+
+          <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8">
+            <div className="relative">
+              <img className="w-16 py-8" src="/img/nodes.svg" alt="Sun icon" />
+              <h3 className="text-3xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">Deploy EventCatalog Anywhere</h3>
+              <div className="space-y-6 md:space-y-10">
+                <p className="mt-3 text-lg text-gray-500">
+                  EventCatalog uses Static Generation to build the catalog and gives you static HTML. This means you can host the
+                  content anywhere you like.
+                </p>
+              </div>
+            </div>
+            <div className="mt-10 -mx-4 relative lg:mt-0 z-20" aria-hidden="true">
+              <svg
+                className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
+                width={784}
+                height={404}
+                fill="none"
+                viewBox="0 0 784 404"
+              >
+                <defs>
+                  <pattern
+                    id="ca9667ae-9f92-4be7-abcb-9e3d727f2941"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                  </pattern>
+                </defs>
+                <rect width={784} height={404} fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)" />
+              </svg>
+              <img
+                id="page-example"
+                width={720}
+                className="px-4 md:px-0 z-90 mx-auto border-gray-400"
+                src="/img/deployment.png"
+                alt=""
+              />
             </div>
           </div>
         </div>
