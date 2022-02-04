4bd20c619d67c018adcec81337e302ce9def326a
Tim <>
2022-02-04 13:33:23 +0100

    WIP - Improve handling of dynamic width

--- packages/eventcatalog/components/Mdx/NodeGraph/NodeGraph.tsx
+++ packages/eventcatalog/components/Mdx/NodeGraph/NodeGraph.tsx
@@ -1,16 +1,5 @@
 import React, { useState } from 'react';
-import ReactFlow, {
-  ReactFlowProvider,
-  addEdge,
-  removeElements,
-  Controls,
-  isNode,
-  Elements,
-  Connection,
-  Edge,
-  NodeExtent,
-  Position,
-} from 'react-flow-renderer';
+import ReactFlow, { ReactFlowProvider, Controls, isNode, Elements, NodeExtent, Position } from 'react-flow-renderer';
 import dagre from 'dagre';
 import { Service, Event } from '@eventcatalog/types';
 
@@ -26,6 +15,24 @@ interface NodeGraphProps {
   isHorizontal?: boolean;
   isDraggable?: boolean;
 }
+const defaultNodeWidth = 150;
+const defaultNodeHeight = 36;
+
+// Helper - ReactFlow canvas height calculator
+const calcCanvasHeight = (data, type): number => {
+  const minHeight = 300;
+  const nodeSpacing = defaultNodeHeight * 2; // Each node has height of 50px, plus a margin of 50px = 100
+  let nodesHeight = 0;
+  if (type === 'event') {
+    nodesHeight = Math.max(data.producers.length, data.consumers.length) * nodeSpacing;
+  } else {
+    nodesHeight = Math.max(data.publishes.length, data.subscribes.length) * nodeSpacing;
+  }
+  return Math.max(minHeight, nodesHeight);
+};
+
+// eslint-disable-next-line no-underscore-dangle
+// const nodeHasDimension = (el) => el.__rf.width && el.__rf.height;
 
 const dagreGraph = new dagre.graphlib.Graph();
 dagreGraph.setDefaultEdgeLabel(() => ({}));
@@ -39,20 +46,29 @@ function NodeGraph({
   data,
   source,
   rootNodeColor,
-  maxHeight = 360,
+  maxHeight,
   maxZoom = 5,
   isAnimated = true,
   isHorizontal = true,
   isDraggable = false,
 }: NodeGraphProps) {
+  // Initial elements for events or services
   const initialElements =
     source === 'event'
       ? buildReactFlowForEvent(data as Event, rootNodeColor, isAnimated)
       : buildReactFlowForService(data as Service, rootNodeColor, isAnimated);
 
+  // Set dynamic height of node graph
+  const dynamicHeight = maxHeight || calcCanvasHeight(data, source);
+
   const [elements, setElements] = useState<Elements>(initialElements);
-  const onConnect = (params: Connection | Edge) => setElements((els) => addEdge(params, els));
-  const onElementsRemove = (elementsToRemove: Elements) => setElements((els) => removeElements(elementsToRemove, els));
+
+  // const nodes = useStoreState((state) => state.nodes);
+  // const edges = useStoreState((state) => state.edges);
+  // const [shouldLayout, setShouldLayout] = useState(true);
+
+  // const onConnect = (params: Connection | Edge) => setElements((els) => addEdge(params, els));
+  // const onElementsRemove = (elementsToRemove: Elements) => setElements((els) => removeElements(elementsToRemove, els));
   const onElementClick = (event, element) => window.open(element.data.link, '_self');
 
   const onLayout = () => {
@@ -61,14 +77,18 @@ function NodeGraph({
 
     elements.forEach((el) => {
       if (isNode(el)) {
-        dagreGraph.setNode(el.id, { width: 150, height: 50 });
+        // dagreGraph.setNode(el.id, { width: 150, height: 50 });
+        dagreGraph.setNode(el.id, {
+          // eslint-disable-next-line no-underscore-dangle
+          width: el.__rf?.width || defaultNodeWidth,
+          // eslint-disable-next-line no-underscore-dangle
+          height: el.__rf?.height || defaultNodeHeight,
+        });
+        console.log('el', el);
       } else {
         dagreGraph.setEdge(el.source, el.target);
       }
     });
-
-    // const nodeWidth = 150;
-    // const nodeHeight = 50;
 
     dagre.layout(dagreGraph);
 
@@ -80,10 +100,10 @@ function NodeGraph({
         // we need to pass a slightly different position in order to notify react flow about the change
         // @TODO how can we change the position handling so that we dont need this hack?
         el.position = {
-          // x: nodeWithPosition.x - nodeWidth / 2 + Math.random() / 1000,
-          // y: nodeWithPosition.y - nodeHeight / 2,
-          x: nodeWithPosition.x + Math.random() / 1000,
-          y: nodeWithPosition.y,
+          x: nodeWithPosition.x + (nodeWithPosition.data?.width || defaultNodeWidth) / 2 + Math.random() / 1000,
+          y: nodeWithPosition.y - (nodeWithPosition.data?.height || defaultNodeHeight) / 2,
+          // x: nodeWithPosition.x + Math.random() / 1000,
+          // y: nodeWithPosition.y,
         };
       }
 
@@ -93,16 +113,31 @@ function NodeGraph({
     setElements(layoutedElements);
   };
 
+  // useEffect(() => {
+  //   if (
+  //     shouldLayout &&
+  //     nodes.length &&
+  //     nodes.length > 0 &&
+  //     nodes.every(nodeHasDimension)
+  //   ) {
+  //     const elements = [...nodes, ...edges];
+  //     const elementsWithLayout = getLayoutedElements(elements);
+  //
+  //     setElements(elementsWithLayout);
+  //     setShouldLayout(false);
+  //   }
+  // }, [shouldLayout, nodes, edges, setElements, setShouldLayout]);
+
   return (
-    <div className="node-graph w-full" style={{ height: maxHeight }}>
+    <div className="node-graph w-full" style={{ height: dynamicHeight }}>
       <ReactFlowProvider>
         <ReactFlow
           elements={elements}
           maxZoom={maxZoom}
           nodesDraggable={isDraggable}
           onElementClick={onElementClick}
-          onConnect={onConnect}
-          onElementsRemove={onElementsRemove}
+          // onConnect={onConnect}
+          // onElementsRemove={onElementsRemove}
           nodeExtent={nodeExtent}
           onLoad={() => onLayout()}
         >
--- packages/eventcatalog/lib/node-graphs.ts
+++ packages/eventcatalog/lib/node-graphs.ts
@@ -1,11 +1,14 @@
 import type { Event, Service } from '@eventcatalog/types';
 
-import { XYPosition } from 'react-flow-renderer';
+import { ArrowHeadType, XYPosition } from 'react-flow-renderer';
 import getConfig from 'next/config';
+
+import { Node, Edge } from 'react-flow-renderer';
 
 const { publicRuntimeConfig: { basePath = '' } = {} } = getConfig();
 
 const generateLink = (value, type) => (basePath !== '' ? `${basePath}/${type}/${value}` : `/${type}/${value}`);
+const calcWidth = (value) => (value.length > 15 ? value.length * 8 : 150);
 
 /**
  * Builds a graph for a given event
@@ -26,48 +29,47 @@ export const buildReactFlowForEvent = (
   const producerColor = '#75d7b6';
 
   // Transforms services & event into a graph model
-  const leftNodes = producers.map((node) => ({
+  const leftNodes: Node[] = producers.map((node) => ({
     id: `l-${node.replace(/ /g, '_')}`,
-    data: { label: node, link: generateLink(node, 'services') },
-    style: { border: `2px solid ${producerColor}` },
+    data: { label: node, link: generateLink(node, 'services'), width: calcWidth(node) },
+    style: { border: `2px solid ${producerColor}`, width: calcWidth(node) },
     type: 'input',
     position,
   }));
-  const rightNodes = consumers.map((node) => ({
+  const rightNodes: Node[] = consumers.map((node) => ({
     id: `r-${node.replace(/ /g, '_')}`,
-    data: { label: node, link: generateLink(node, 'services') },
-    style: { border: `2px solid ${consumerColor}` },
+    data: { label: node, link: generateLink(node, 'services'), width: calcWidth(node) },
+    style: { border: `2px solid ${consumerColor}`, width: calcWidth(node) },
     type: 'output',
     position,
   }));
-  const centerNode = {
+  const centerNode: Node = {
     id: `c-${eventName.replace(/ /g, '_')}`,
-    data: { label: eventName, link: generateLink(eventName, 'events') },
-    style: { border: `2px solid ${rootNodeColor}` },
+    data: { label: eventName, link: generateLink(eventName, 'events'), width: calcWidth(eventName) },
+    style: { border: `2px solid ${rootNodeColor}`, width: calcWidth(eventName) },
     position,
   };
 
   // Build connections
-  const connectionsLeft = producers.map((node) => ({
+  const edgesLeft: Edge[] = producers.map((node) => ({
     id: `el-${node.replace(/ /g, '_')}`,
     source: `l-${node.replace(/ /g, '_')}`,
     target: `c-${eventName.replace(/ /g, '_')}`,
     type: 'smoothstep',
-    arrowHeadType: 'arrowclosed',
+    arrowHeadType: ArrowHeadType.ArrowClosed,
     animated: isAnimated,
   }));
-  const connectionsRight = consumers.map((node) => ({
+  const edgesRight: Edge[] = consumers.map((node) => ({
     id: `er-${node.replace(/ /g, '_')}`,
     target: `r-${node.replace(/ /g, '_')}`,
     source: `c-${eventName.replace(/ /g, '_')}`,
     type: 'smoothstep',
-    arrowHeadType: 'arrowclosed',
+    arrowHeadType: ArrowHeadType.ArrowClosed,
     animated: isAnimated,
   }));
 
   // Merge nodes in order
-  const elements = [...leftNodes, centerNode, ...rightNodes, ...connectionsLeft, ...connectionsRight];
-
+  const elements: (Node | Edge)[] = [...leftNodes, centerNode, ...rightNodes, ...edgesLeft, ...edgesRight];
   return elements;
 };
 
@@ -88,21 +90,21 @@ export const buildReactFlowForService = (
   const subscribeColor = '#75d7b6';
 
   // Transforms services & event into a graph model
-  const leftNodes = publishes.map((node) => ({
+  const leftNodes: Node[] = publishes.map((node) => ({
     id: `l-${node.name.replace(/ /g, '_')}`,
-    data: { label: node.name, link: generateLink(node.name, 'events') },
-    style: { border: `2px solid ${publishColor}` },
+    data: { label: node.name, link: generateLink(node.name, 'events'), width: calcWidth(node.name) },
+    style: { border: `2px solid ${publishColor}`, width: calcWidth(node.name) },
     type: 'output',
     position,
   }));
-  const rightNodes = subscribes.map((node) => ({
+  const rightNodes: Node[] = subscribes.map((node) => ({
     id: `r-${node.name.replace(/ /g, '_')}`,
     data: { label: node.name, link: generateLink(node.name, 'events') },
     style: { border: `2px solid ${subscribeColor}` },
     type: 'input',
     position,
   }));
-  const centerNode = {
+  const centerNode: Node = {
     id: `c-${serviceName.replace(/ /g, '_')}`,
     data: { label: serviceName, link: generateLink(serviceName, 'services') },
     style: { border: `2px solid ${rootNodeColor}` },
@@ -110,25 +112,24 @@ export const buildReactFlowForService = (
   };
 
   // Build connections
-  const connectionsLeft = publishes.map((node) => ({
+  const edgesLeft: Edge[] = publishes.map((node) => ({
     id: `el-${node.name.replace(/ /g, '_')}`,
     target: `l-${node.name.replace(/ /g, '_')}`,
     source: `c-${serviceName.replace(/ /g, '_')}`,
     type: 'smoothstep',
-    arrowHeadType: 'arrowclosed',
+    arrowHeadType: ArrowHeadType.ArrowClosed,
     animated: isAnimated,
   }));
-  const connectionsRight = subscribes.map((node) => ({
+  const edgesRight: Edge[] = subscribes.map((node) => ({
     id: `er-${node.name.replace(/ /g, '_')}`,
     source: `r-${node.name.replace(/ /g, '_')}`,
     target: `c-${serviceName.replace(/ /g, '_')}`,
     type: 'smoothstep',
-    arrowHeadType: 'arrowclosed',
+    arrowHeadType: ArrowHeadType.ArrowClosed,
     animated: isAnimated,
   }));
 
   // Merge nodes in order
-  const elements = [...leftNodes, centerNode, ...rightNodes, ...connectionsLeft, ...connectionsRight];
-
+  const elements: (Node | Edge)[] = [...leftNodes, centerNode, ...rightNodes, ...edgesLeft, ...edgesRight];
   return elements;
 };
