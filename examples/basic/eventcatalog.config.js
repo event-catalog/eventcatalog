dbda129d4f84c914e2f654b98af429a924f8ee6f
thim81 <timhaselaars@gmail.com>
2022-02-04 08:25:42 +0100

    Update examples/basic/eventcatalog.config.js
    Co-authored-by: David Boyne <boyneyy123@gmail.com>

--- examples/basic/eventcatalog.config.js
+++ examples/basic/eventcatalog.config.js
@@ -3,7 +3,6 @@ module.exports = {
   tagline: 'Discover, Explore and Document your Event Driven Architectures',
   organizationName: 'Your Company',
   editUrl: 'https://github.com/boyney123/eventcatalog-demo/edit/master',
-  homepageLink: 'https://eventcatalog.dev/',
   trailingSlash: true,
   logo: {
     alt: 'EventCatalog Logo',
