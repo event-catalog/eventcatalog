1adb52fd50edc04c5d1e819aed6f343b27f733ce
David Boyne <davidboyne@Davids-MBP.lan>
2022-02-07 16:22:04 +0000

    feat: added better code example component

--- packages/eventcatalog/components/SyntaxHighlighter.tsx
+++ packages/eventcatalog/components/SyntaxHighlighter.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import codeStyle from 'react-syntax-highlighter/dist/cjs/styles/prism/dracula';
 import { Prism as PrismSyntaxHighlighter } from 'react-syntax-highlighter';
 import copy from 'copy-text-to-clipboard';
+import { ClipboardCopyIcon } from '@heroicons/react/outline';
 
 function SyntaxHighlighter({ language, name = '', ...props }: any) {
   const [showCopied, setShowCopied] = useState(false);
@@ -15,18 +16,25 @@ function SyntaxHighlighter({ language, name = '', ...props }: any) {
 
   const regex = /\\n/g;
   return (
-    <div className="relative group">
-      <button
-        type="button"
-        onClick={handleCopyCode}
-        className="absolute top-2  right-5 text-sm bg-gray-700 text-white rounded-md py-1 px-4 transform transition opacity-0  group-hover:opacity-100"
-      >
-        {showCopied ? 'Copied' : 'Copy'}
-      </button>
-      <PrismSyntaxHighlighter style={codeStyle} language={language} {...props} wrapLines className="max-h-96 overflow-auto">
+    <div className="h-96 relative">
+      {name && (
+        <div className={`flex -mb-3 pb-2 bg-gray-900 ${name ? 'justify-between' : 'justify-end'}`}>
+          {name && <span className="block text-xs py-2 px-2 font-bold text-gray-300">{name}</span>}
+          <button type="button" onClick={handleCopyCode} className="text-xs  text-white rounded-md px-4">
+            {showCopied ? 'Copied' : <ClipboardCopyIcon className="h-4 w-4" />}
+          </button>
+        </div>
+      )}
+
+      {!name && (
+        <button type="button" onClick={handleCopyCode} className="text-xs absolute right-0 top-2 text-white rounded-md px-4">
+          {showCopied ? 'Copied' : <ClipboardCopyIcon className="h-4 w-4" />}
+        </button>
+      )}
+
+      <PrismSyntaxHighlighter style={codeStyle} language={language} {...props} wrapLines className="h-96 overflow-auto">
         {props.children.replace(regex, '\n')}
       </PrismSyntaxHighlighter>
-      {name && <span className="-mb-2 block text-xs text-right font-bold">{name}</span>}
     </div>
   );
 }
