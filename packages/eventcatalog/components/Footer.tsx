8a3464636fee1b85170fdd8ca25523cf507f6535
David Boyne <davidboyne@Davids-MBP.lan>
2022-02-09 10:07:29 +0000

    chore: added new item for visualiser

--- packages/eventcatalog/components/Footer.tsx
+++ packages/eventcatalog/components/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
           <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
             {navigation.map((item) => (
               <div key={item.label} className="px-5 py-2">
-                <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
+                <a href={item.href} className="text-base text-gray-500 hover:text-gray-400">
                   {item.label}
                 </a>
               </div>
