5724377e7b27aad25a2fa138af39b2c857398532
David Boyne <davidboyne@Davids-MBP.lan>
2022-04-14 11:25:29 +0100

    fix: build issues with eventcatalog core

--- packages/eventcatalog/components/Mdx/OpenApiSpec.tsx
+++ packages/eventcatalog/components/Mdx/OpenApiSpec.tsx
@@ -1,7 +1,7 @@
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
 import 'swagger-ui-react/swagger-ui.css';
 
-const SwaggerUI = dynamic(import('swagger-ui-react'), {ssr: false})
+const SwaggerUI = dynamic(import('swagger-ui-react'), { ssr: false });
 
 interface OpenApiSpecProps {
   spec: string;
