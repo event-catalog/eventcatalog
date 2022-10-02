3bc5bdf3c85f2fea255b172dfa146282e3826e27
Boyne <dboyne@3c06304f253a.ant.amazon.com>
2022-10-02 21:34:04 +0100

    chore: updating to minify files and change cdn for font-awesome

--- packages/eventcatalog/pages/_document.tsx
+++ packages/eventcatalog/pages/_document.tsx
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <script src="//unpkg.com/three" />
+        <script src="//unpkg.com/three@0.145.0/build/three.min.js" />
         <script src="//unpkg.com/three/examples/js/renderers/CSS2DRenderer.js" />
-        <link rel="stylesheet" href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/styles/atom-one-light.min.css" />
       </Head>
       <body>
