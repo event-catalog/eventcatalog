34358f5b55fa58a8502a81ed44fb9c4b1901bdd2
Matt Martz <matt.martz@gmail.com>
2022-10-20 02:39:35 -0400

    Feature: Add Badge Filtering to Domains, Events, and Services (#314)
    Co-authored-by: Boyne <dboyne@3c06304f253a.ant.amazon.com>
    Co-authored-by: David Boyne <boyneyy123@gmail.com>

--- packages/eventcatalog/pages/services.tsx
+++ packages/eventcatalog/pages/services.tsx
@@ -26,12 +26,61 @@ export interface PageProps {
 export default function Page({ services }: PageProps) {
   const [servicesToRender, setServicesToRender] = useState(services);
   const [searchFilter, setSearchFilter] = useState('');
+  const [selectedFilters, setSelectedFilters] = useState({ badges: [] });
+
+  const filters = [
+    {
+      id: 'badges',
+      name: `Filter by Badges`,
+      options: services.reduce((p, c) => {
+        if (!c.badges) {
+          return p;
+        }
+        c.badges.forEach((badge) => {
+          const existing = p.map((b) => b.value);
+          if (!existing.includes(badge.content)) {
+            p.push({
+              value: badge.content,
+              label: badge.content,
+              checked: false,
+            });
+          }
+        });
+        return p;
+      }, [] as { value: string; label: string; checked: boolean }[]),
+    },
+  ];
+
+  const handleFilterSelection = (option, type, event) => {
+    console.log(option, type, event);
+
+    if (event.target.checked) {
+      const newFilters = selectedFilters[type].concat([option.value]);
+      setSelectedFilters({ ...selectedFilters, [type]: newFilters });
+    } else {
+      const newFilters = selectedFilters[type].filter((value) => value !== option.value);
+      setSelectedFilters({ ...selectedFilters, [type]: newFilters });
+    }
+  };
 
   const getFilteredServices = (): any => {
     let filteredServices = services;
 
+    if (!selectedFilters.badges && !searchFilter) return services;
+
     if (searchFilter) {
       filteredServices = filteredServices.filter((service) => service.name.toLowerCase().includes(searchFilter.toLowerCase()));
+    }
+
+    if (selectedFilters.badges.length > 0) {
+      const { badges: badgeFilters } = selectedFilters;
+      // @ts-ignore
+      filteredServices = filteredServices.filter((event) => {
+        if (!event.badges) {
+          return false;
+        }
+        return event.badges.filter((badge) => badgeFilters.includes(badge.content)).length !== 0;
+      });
     }
 
     return filteredServices;
@@ -40,7 +89,7 @@ export default function Page({ services }: PageProps) {
   useEffect(() => {
     setServicesToRender(getFilteredServices());
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchFilter]);
+  }, [selectedFilters, searchFilter]);
 
   const debouncedFilter = useCallback(
     debounce((e) => {
@@ -49,7 +98,7 @@ export default function Page({ services }: PageProps) {
     [servicesToRender]
   );
 
-  const filtersApplied = !!searchFilter;
+  const filtersApplied = !!searchFilter || selectedFilters.badges.length > 0;
   const [showMermaidDiagrams, setShowMermaidDiagrams] = useState(false);
   const { title } = useConfig();
 
@@ -129,6 +178,41 @@ export default function Page({ services }: PageProps) {
                   />
                 </div>
               </div>
+
+              {filters.map((section: any) => {
+                if (!section.options.length) return null;
+                return (
+                  <div key={section.id} className="border-b border-gray-200 py-6">
+                    <h3 className="-my-3 flow-root">
+                      <div className="py-3 bg-white w-full flex items-center justify-between text-sm text-gray-400 hover:text-gray-500">
+                        <span className="font-bold font-medium text-gray-900">
+                          {section.name} ({section.options.length})
+                        </span>
+                      </div>
+                    </h3>
+                    <div className="pt-6">
+                      <div className="space-y-4">
+                        {section.options.map((option, optionIdx) => (
+                          <div key={option.value} className="flex items-center">
+                            <input
+                              id={`filter-${section.id}-${optionIdx}`}
+                              name={`${section.id}[]`}
+                              defaultValue={option.value}
+                              type="checkbox"
+                              onChange={(event) => handleFilterSelection(option, section.id, event)}
+                              defaultChecked={option.checked}
+                              className="h-4 w-4 border-gray-300 rounded text-gray-600 focus:ring-gray-500"
+                            />
+                            <label htmlFor={`filter-${section.id}-${optionIdx}`} className="ml-3 text-sm text-gray-600">
+                              {option.label}
+                            </label>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
 
               <div className="border-b border-gray-200 pb-6">
                 <h3 className="-my-3 flow-root">
