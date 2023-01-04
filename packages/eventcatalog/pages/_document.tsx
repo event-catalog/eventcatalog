c59eabc430464d39616261b36404a7fc4076af60
Michel Grootjans <michel.grootjans@gmail.com>
2023-01-04 13:45:44 +0100

    fix three.js version to 0.145.0

--- packages/eventcatalog/pages/_document.tsx
+++ packages/eventcatalog/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <script src="//unpkg.com/three@0.145.0/build/three.min.js" />
-        <script src="//unpkg.com/three/examples/js/renderers/CSS2DRenderer.js" />
+        <script src="//unpkg.com/three@0.145.0/examples/js/renderers/CSS2DRenderer.js" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/styles/atom-one-light.min.css" />
       </Head>
