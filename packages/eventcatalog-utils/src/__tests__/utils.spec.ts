ec0cf1caaa0d3529e5651403fb7cb334a74467ee
Boyne <dboyne@3c06304f253a.ant.amazon.com>
2023-01-16 14:33:36 +0000

    fix for 260 - nodegraphs used over mermaid graphs when writing to eventcatalog

--- packages/eventcatalog-utils/src/__tests__/utils.spec.ts
+++ packages/eventcatalog-utils/src/__tests__/utils.spec.ts
@@ -353,7 +353,7 @@ describe('eventcatalog-utils', () => {
 
         const result = fs.readFileSync(path.join(eventPath, 'index.md'), 'utf-8');
 
-        console.log('result', result)
+        console.log('result', result);
 
         expect(result).toMatchMarkdown(`
         ---
@@ -1024,7 +1024,7 @@ describe('eventcatalog-utils', () => {
 
         const result = fs.readFileSync(path.join(domainPath, 'index.md'), 'utf-8');
 
-        console.log('result', result)
+        console.log('result', result);
 
         expect(result).toMatchMarkdown(`
         ---
