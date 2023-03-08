e5d26dc389f6e4ab8ffcc8e8ee04b8c51d8d1ff9
Boyne <dboyne@3c06304f253a.ant.amazon.com>
2023-03-08 12:13:26 +0000

    fixing docs deployment

--- packages/eventcatalog/tailwind.config.js
+++ packages/eventcatalog/tailwind.config.js
@@ -18,7 +18,21 @@ module.exports = {
       'bg-green-500',
       'bg-indigo-500',
       'bg-yellow-500',
+      'bg-purple-100',
+      'text-purple-800',
+      'bg-pink-100',
+      'text-pink-800',
+      'bg-green-100',
+      'text-green-800',
+      'bg-yellow-100',
+      'text-yellow-800',
+      'bg-blue-100',
+      'text-blue-800',
+      'bg-indigo-100',
+      'text-indigo-800',
     ],
   },
   plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms'), require('@tailwindcss/line-clamp')],
 };
+
+
