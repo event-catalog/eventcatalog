f3694a4fa8d5a2ce476ee9a297484342b26097f1
Boyne <dboyne@3c06304f253a.ant.amazon.com>
2023-09-18 13:46:50 +0100

    fixing style issues and also adding link

--- examples/basic/eventcatalog.config.js
+++ examples/basic/eventcatalog.config.js
@@ -48,20 +48,6 @@ module.exports = {
       summary: 'Currently building tools for Event Architectures.'
     },
     {
-      id: 'dboyne2',
-      name: 'David Boyne',
-      avatarUrl: 'https://pbs.twimg.com/profile_images/1262283153563140096/DYRDqKg6_400x400.png',
-      role: 'Developer',
-      summary: 'Currently building tools for Event Architectures.'
-    },
-    {
-      id: 'dboyne3',
-      name: 'David Boyne',
-      avatarUrl: 'https://pbs.twimg.com/profile_images/1262283153563140096/DYRDqKg6_400x400.png',
-      role: 'Developer',
-      summary: 'Currently building tools for Event Architectures Currently building tools for Event Architectures Currently building tools for Event Architectures Currently building tools for Event Architectures Currently building tools for Event Architectures Currently building tools for Event Architectures Currently building tools for Event Architectures.'
-    },
-    {
       id: 'mSmith',
       name: 'Matthew Smith',
       avatarUrl: 'https://randomuser.me/api/portraits/lego/3.jpg',
