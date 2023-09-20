6ef64801d97a81fbaa0711a5e0dd97e7dc55a89c
stanislav.petrosyan <stanislav.petrosyan@cherrychain.it>
2023-09-20 09:44:26 +0200

    added build mermaid for domain test

--- packages/eventcatalog/lib/__tests__/graphs.spec.ts
+++ packages/eventcatalog/lib/__tests__/graphs.spec.ts
@@ -1,4 +1,5 @@
-import { buildMermaidFlowChartForEvent, buildMermaidFlowChartForService } from '../graphs';
+import { Domain } from 'domain';
+import { buildMermaidFlowChartForDomain, buildMermaidFlowChartForEvent, buildMermaidFlowChartForService } from '../graphs';
 
 jest.mock('next/config', () => () => ({
   publicRuntimeConfig: {
@@ -65,6 +66,40 @@ describe('graphs', () => {
       const result = buildMermaidFlowChartForEvent(event);
       expect(result).toContain(`Service_1_With_Spaces[Service 1 With Spaces]:::producer-->My_Event[My Event]:::event`);
       expect(result).not.toContain(`Service 1 With Spaces[`);
+    });
+  });
+  describe('buildMermaidFlowChartForDomain', () => {
+    it('takes a Domain and returns the mermaid code showing relationships between the event and its consumers and producers', () => {
+      const event = { name: 'My Event', version: '0.0.1' };
+      const event2 = { name: 'My Event 2', version: '0.0.1' };
+
+      const service = {
+        id: 'My Service',
+        name: 'My Service',
+        version: '0.0.1',
+        summary: 'Summary',
+        publishes: [event],
+        subscribes: [event2],
+      };
+
+      const domain = {
+        name: 'My Domain',
+        summary: 'aSummary',
+        services: [service],
+      };
+
+      const result = buildMermaidFlowChartForDomain(domain);
+
+      expect(result).toContain('l-My_Event_2[My Event 2]:::producer-->My_Service[My Service]:::event');
+      expect(result).toContain('click l-My_Event_2 href "/docs/events/My Event 2" "Go to My Event 2" _self');
+      expect(result).toContain(
+        'click My_Service href "/docs/domains/[object Object]/services/My Service" "Go to My Service" _self'
+      );
+      expect(result).toContain('My_Service[My Service]:::event-->r-My_Event[My Event]:::consumer');
+      expect(result).toContain('click r-My_Event href "/docs/events/My Event" "Go to My Event" _self');
+      expect(result).toContain('classDef event stroke:#2563eb,stroke-width: 4px;');
+      expect(result).toContain('classDef producer stroke:#75d7b6,stroke-width: 2px;');
+      expect(result).toContain('classDef consumer stroke:#818cf8,stroke-width: 2px;');
     });
   });
 });
