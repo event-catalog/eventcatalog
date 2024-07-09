c8ce1cf6db7e14eb145eba9ab175bcd64eeb51d7
David Boyne <davidboyne@Davids-MBP.upp.com>
2024-07-09 22:28:19 +0100

    adding types for config files

--- bin/eventcatalog.config.ts
+++ bin/eventcatalog.config.ts
@@ -1,25 +1,25 @@
 type SideBarConfig = {
-    visible: boolean
-}
+  visible: boolean;
+};
 
 export interface Config {
-  title: string
-  tagline: false
-  organizationName: string
-  homepageLink: string
-  editUrl: string
+  title: string;
+  tagline: false;
+  organizationName: string;
+  homepageLink: string;
+  editUrl: string;
   logo: {
-    alt: string
-    src: string
-  }
+    alt: string;
+    src: string;
+  };
   docs: {
     sidebar: {
-        showPageHeadings: true
-        services?: SideBarConfig
-        messages?: SideBarConfig
-        domains?: SideBarConfig
-        teams?: SideBarConfig
-        users?: SideBarConfig 
-    }
-  }
+      showPageHeadings: true;
+      services?: SideBarConfig;
+      messages?: SideBarConfig;
+      domains?: SideBarConfig;
+      teams?: SideBarConfig;
+      users?: SideBarConfig;
+    };
+  };
 }
