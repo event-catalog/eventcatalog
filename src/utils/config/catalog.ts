1c42b9f57ecabb8ee7745cf4d98a23e7d5e2a527
David Boyne <boyneyy123@gmail.com>
2024-07-09 07:31:47 +0100

    feat(core): added ability to hide resources in sidebar (#565)
    * added ability to hide items in sidebar
    
    * Create serious-sloths-retire.md
    
    ---------
    
    Co-authored-by: David Boyne <davidboyne@Davids-MBP.upp.com>

--- src/utils/config/catalog.ts
+++ src/utils/config/catalog.ts
@@ -1,2 +1,20 @@
 import * as config from '@config';
+
+type SideBarItemConfig = {
+  visible?: boolean;
+};
+
+export type CatalogConfig = {
+  docs: {
+    sidebar: {
+      showPageHeadings?: boolean;
+      domains: SideBarItemConfig;
+      services: SideBarItemConfig;
+      messages: SideBarItemConfig;
+      teams: SideBarItemConfig;
+      users: SideBarItemConfig;
+    };
+  };
+};
+
 export default config.default;
