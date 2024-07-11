bdd88d42e3ccea1ad9e9a88b25acc2801a1bd220
David Boyne <davidboyne@Davids-MBP.upp.com>
2024-07-11 22:40:28 +0100

    add support for base url and slashes

--- src/utils/url-builder.ts
+++ src/utils/url-builder.ts
@@ -1,27 +1,26 @@
 import config from '@eventcatalog';
 
 const cleanUrl = (url: string) => {
-    return url.replace(/\/+/g, '/');
-}
+  return url.replace(/\/+/g, '/');
+};
 
 // Custom URL builder as Astro does not support this stuff out the box
 export const buildUrl = (url: string, ignoreTrailingSlash = false) => {
-    // Should a trailingSlash be added to urls?
-    const trailingSlash = config.trailingSlash || false;
+  // Should a trailingSlash be added to urls?
+  const trailingSlash = config.trailingSlash || false;
 
-    let newUrl = url;
+  let newUrl = url;
 
-    // If the base URL is not the root, we need to append it
-    if(import.meta.env.BASE_URL !== '/') {
-        newUrl = `${import.meta.env.BASE_URL}${url}`;
-    }
+  // If the base URL is not the root, we need to append it
+  if (import.meta.env.BASE_URL !== '/') {
+    newUrl = `${import.meta.env.BASE_URL}${url}`;
+  }
 
-    // Should we add a trailing slash to the url?
-    if(trailingSlash && !ignoreTrailingSlash){
-        if(url.endsWith('/')) return newUrl;
-        return cleanUrl(`${newUrl}/`);
-    }
+  // Should we add a trailing slash to the url?
+  if (trailingSlash && !ignoreTrailingSlash) {
+    if (url.endsWith('/')) return newUrl;
+    return cleanUrl(`${newUrl}/`);
+  }
 
-    
-    return cleanUrl(newUrl);
-}
+  return cleanUrl(newUrl);
+};
