27b15b10f27eda3abb795cf357ad001d08e60a82
David Boyne <davidboyne@Davids-MBP.upp.com>
2024-07-19 12:35:39 +0100

    fix(core): fixed search, now set output path on build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,20 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import pagefind from "astro-pagefind";
 import { mermaid } from "./src/remark-plugins/mermaid"
+import { join } from 'node:path';
 
 /** @type {import('bin/eventcatalog.config').Config} */
 import config from './eventcatalog.config';
 import expressiveCode from 'astro-expressive-code';
 
+const coreDirectory = process.env.CATALOG_DIR || process.cwd();
+
 // https://astro.build/config
 export default defineConfig({
   base: config.base || '/',
   server: { port: config.port || 3000 },
+
+  outDir: join(coreDirectory, 'dist'),
 
   // https://docs.astro.build/en/reference/configuration-reference/#site
   site: config.homepageLink || 'https://eventcatalog.dev/',
@@ -35,7 +40,11 @@ export default defineConfig({
       remarkPlugins: [mermaid],
       gfm: false,
     }),
-    pagefind(),
+    pagefind({
+
+    }),
+
+    
   ],
   redirects: {
     "/": "/docs"
