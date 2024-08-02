103f52dacffa85b5ba6b347e64fb65853755932c
carlosallexandre <carlos_costarodrigues@outlook.com>
2024-08-02 11:15:30 -0300

    fix: flat build-cd command to set envs and run build commands in the same process
    It turns the build:cd command in the execution of build-ci script only. This way
    build commands, such as `astro build`, run in the same process (created by execSync)
    where the envs are set.

--- scripts/build-ci.js
+++ scripts/build-ci.js
@@ -16,7 +16,7 @@ fs.copyFileSync(join(projectDIR, 'eventcatalog.config.js'), join(catalogDir, 'ev
 
 fs.copyFileSync(join(projectDIR, 'eventcatalog.styles.css'), join(catalogDir, 'eventcatalog.styles.css'));
 
-execSync(`cross-env NODE_ENV=CI PROJECT_DIR=${projectDIR} CATALOG_DIR=${catalogDir} npm run scripts:hydrate-content`, {
+execSync(`cross-env NODE_ENV=CI PROJECT_DIR=${projectDIR} CATALOG_DIR=${catalogDir} npm run build`, {
   cwd: catalogDir,
   stdio: 'inherit',
 });
