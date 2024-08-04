5cce25a8713290ea37c94a0706c8ab08c2abfa7e
David Boyne <davidboyne@Davids-MBP.upp.com>
2024-08-04 20:55:23 +0100

    chore(core): fixing project tests

---
+++ src/utils/__tests__/events/node-graph.speca.js
@@ -0,0 +1,152 @@
+import { getNodesAndEdges } from '../../events/node-graph';
+import { getCollection } from 'astro:content';
+
+const mockServices = [
+  {
+    id: 'OrderService',
+    slug: 'OrderService',
+    collection: 'services',
+    data: {
+      id: 'OrderService',
+      version: '0.0.1',
+      sends: [
+        {
+          id: 'OrderCreatedEvent',
+          version: '0.0.1',
+        },
+      ],
+    },
+  },
+  {
+    id: 'PaymentService',
+    slug: 'PaymentService',
+    collection: 'services',
+    data: {
+      id: 'PaymentService',
+      version: '0.0.1',
+      receives: [
+        {
+          id: 'OrderCreatedEvent',
+          version: '0.0.1',
+        },
+      ],
+    },
+  },
+];
+const mockEvents = [
+  {
+    id: 'OrderCreatedEvent',
+    slug: 'OrderCreatedEvent',
+    collection: 'events',
+    data: {
+      id: 'OrderCreatedEvent',
+      version: '0.0.1',
+    },
+  },
+];
+
+const mockCollections = () => {
+  getCollection.mockImplementation((key, filterFn) => {
+    if (key === 'services') {
+      return Promise.resolve(mockServices);
+    }
+    if (key === 'events') {
+      return Promise.resolve(mockEvents);
+    }
+    return Promise.resolve([]);
+  });
+};
+
+describe('Services NodeGraph', () => {
+  beforeEach(() => {
+    getCollection.mockClear();
+  });
+
+  describe('getNodesAndEdges', () => {
+    it('should return nodes and edges for a given event', async () => {
+      // getCollection.mockResolvedValueOnce(mockServices);
+      mockCollections();
+
+      const { nodes, edges } = await getNodesAndEdges({ id: 'OrderCreatedEvent', version: '0.0.1' });
+
+      // The middle node itself, the service
+      const expectedEventNode = {
+        id: 'OrderCreatedEvent-0.0.1',
+        sourcePosition: 'right',
+        targetPosition: 'left',
+        // data: { mode: 'simple', message: expect.anything(), showTarget: false, showSource: false },
+        data: expect.anything(),
+        position: { x: expect.any(Number), y: expect.any(Number) },
+        type: 'events',
+      };
+
+      const expectedProducerNode = {
+        id: 'OrderService-0.0.1',
+        type: 'services',
+        sourcePosition: 'right',
+        targetPosition: 'left',
+        data: { mode: 'simple', service: mockServices[0], showTarget: false },
+        position: { x: expect.any(Number), y: expect.any(Number) },
+      };
+
+      const expectedConsumerNode = {
+        id: 'PaymentService-0.0.1',
+        sourcePosition: 'right',
+        targetPosition: 'left',
+        data: {
+          title: 'PaymentService',
+          mode: 'simple',
+          service: mockServices[1],
+          showSource: false,
+        },
+        position: { x: expect.any(Number), y: expect.any(Number) },
+        type: 'services',
+      };
+
+      const expectedEdges = [
+        {
+          id: 'OrderService-0.0.1-OrderCreatedEvent-0.0.1',
+          source: 'OrderService-0.0.1',
+          target: 'OrderCreatedEvent-0.0.1',
+          type: 'smoothstep',
+          label: 'publishes event',
+          animated: false,
+          markerEnd: { type: 'arrow' },
+        },
+        {
+          id: 'OrderCreatedEvent-0.0.1-PaymentService-0.0.1',
+          source: 'OrderCreatedEvent-0.0.1',
+          target: 'PaymentService-0.0.1',
+          type: 'smoothstep',
+          label: 'subscribed by',
+          animated: false,
+          markerEnd: { type: 'arrow' },
+        },
+      ];
+
+      expect(nodes).toEqual(
+        expect.arrayContaining([
+          // Nodes on the left
+          expect.objectContaining(expectedConsumerNode),
+
+          // The event node itself
+          expect.objectContaining(expectedEventNode),
+
+          // Nodes on the right
+          expect.objectContaining(expectedProducerNode),
+        ])
+      );
+
+      expect(edges).toEqual(expectedEdges);
+    });
+
+    it('returns empty nodes and edges if no event is found', async () => {
+      mockCollections();
+
+      const { nodes, edges } = await getNodesAndEdges({ id: 'UnknownEvent' });
+
+      expect(nodes).toEqual([]);
+      expect(edges).toEqual([]);
+    });
+  });
+});
