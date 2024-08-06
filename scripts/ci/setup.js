f9cfd3097911e4b331aa2d3a83424e3ecec95879
David Boyne <davidboyne@Davids-MBP.upp.com>
2024-08-06 21:33:16 +0100

    fixing tests

--- scripts/ci/setup.js
+++ scripts/ci/setup.js
@@ -11,7 +11,7 @@ const args = process.argv.slice(2);
 const catalog = args[0] || 'default';
 
 const catalogDir = join(__dirname, '../');
-const projectDIR = join(__dirname, `../examples/${catalog}`);
+const projectDIR = join(__dirname, `../../examples/${catalog}`);
 
 fs.copyFileSync(join(projectDIR, 'eventcatalog.config.js'), join(catalogDir, 'eventcatalog.config.js'));
 fs.copyFileSync(join(projectDIR, 'eventcatalog.styles.css'), join(catalogDir, 'eventcatalog.styles.css'));
