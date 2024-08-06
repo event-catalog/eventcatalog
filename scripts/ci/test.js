b29f121ef85f0eacb1428aaa679a068aa07e0927
David Boyne <davidboyne@Davids-MBP.upp.com>
2024-08-06 21:37:55 +0100

    fixing tests

---
+++ scripts/ci/test.js
@@ -0,0 +1,22 @@
+/**
+ * Script for GitHub Actions
+ * Sets up the project with required files
+ */
+
+import { join } from 'node:path';
+import fs from 'fs';
+const __dirname = import.meta.dirname;
+
+const args = process.argv.slice(2);
+const catalog = args[0] || 'default';
+
+const catalogDir = join(__dirname, '../');
+const projectDIR = join(__dirname, `../../examples/${catalog}`);
+
+fs.copyFileSync(join(projectDIR, 'eventcatalog.config.js'), join(catalogDir, 'eventcatalog.config.js'));
+fs.copyFileSync(join(projectDIR, 'eventcatalog.styles.css'), join(catalogDir, 'eventcatalog.styles.css'));
+
+execSync(`cross-env NODE_ENV=development PROJECT_DIR=${projectDIR} CATALOG_DIR=${catalogDir} npm run test`, {
+  cwd: catalogDir,
+  stdio: 'inherit',
+});
