4bb943901b7ac7c3b7333609f2b009d1650c8e1e
David Boyne <davidboyne@Davids-MBP.upp.com>
2024-08-06 21:51:26 +0100

    fixing tests

--- scripts/ci/test.js
+++ scripts/ci/test.js
@@ -8,13 +8,12 @@
 import { join } from 'node:path';
 import fs from 'fs';
 import { execSync } from 'node:child_process';
-const __dirname = import.meta.dirname;
 
 const args = process.argv.slice(2);
 const catalog = args[0] || 'default';
 
-const catalogDir = join(__dirname, '../../');
-const projectDIR = join(__dirname, `../../examples/${catalog}`);
+const catalogDir = join(process.cwd());
+const projectDIR = join(process.cwd(), `/examples/${catalog}`);
 
 fs.copyFileSync(join(projectDIR, 'eventcatalog.config.js'), join(catalogDir, 'eventcatalog.config.js'));
 fs.copyFileSync(join(projectDIR, 'eventcatalog.styles.css'), join(catalogDir, 'eventcatalog.styles.css'));
