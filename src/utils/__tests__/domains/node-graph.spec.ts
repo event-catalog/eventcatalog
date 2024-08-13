782e8eb6540c9bbfef44223a92b6864c270593bd
carlosallexandre <carlos_costarodrigues@outlook.com>
2024-08-12 21:23:46 -0300

    fix: find services versions that satisfies semver before get nodes and edges

---
+++ src/utils/__tests__/domains/node-graph.spec.ts
@@ -0,0 +1,234 @@
+import type { ContentCollectionKey } from 'astro:content';
+import { expect, describe, it, vi } from 'vitest';
+import { mockDomains, mockServices, mockEvents, mockCommands } from './mocks';
+import { getNodesAndEdges } from '../../domains/node-graph';
+
+vi.mock('astro:content', async (importOriginal) => {
+  return {
+    ...(await importOriginal<typeof import('astro:content')>()),
+    // this will only affect "foo" outside of the original module
+    getCollection: (key: ContentCollectionKey) => {
+      switch (key) {
+        case 'domains':
+          return Promise.resolve(mockDomains);
+        case 'services':
+          return Promise.resolve(mockServices);
+        case 'events':
+          return Promise.resolve(mockEvents);
+        case 'commands':
+          return Promise.resolve(mockCommands);
+        default:
+          return Promise.resolve([]);
+      }
+    },
+  };
+});
+
+describe('Domains NodeGraph', () => {
+  describe('getNodesAndEdges', () => {
+    it('returns an empty array if no domains are found', async () => {
+      const { nodes, edges } = await getNodesAndEdges({ id: 'UnknownDomain', version: '1.0.0' });
+
+      expect(nodes).toEqual([]);
+      expect(edges).toEqual([]);
+    });
+
+    it('should return nodes and edges for a given domain', async () => {
+      const { nodes, edges } = await getNodesAndEdges({ id: 'Shipping', version: '0.0.1' });
+
+      const expectedServiceNode = {
+        id: 'LocationService-0.0.1',
+        type: 'services',
+        sourcePosition: 'right',
+        targetPosition: 'left',
+        data: { mode: 'simple', service: mockServices[0], showSource: false, showTarget: true },
+        position: { x: expect.any(Number), y: expect.any(Number) },
+      };
+
+      const expectedEventNode = {
+        id: 'OrderPlaced-0.0.1',
+        sourcePosition: 'right',
+        targetPosition: 'left',
+        data: { mode: 'simple', message: mockEvents[0], showTarget: true },
+        position: { x: expect.any(Number), y: expect.any(Number) },
+        type: 'events',
+      };
+
+      const expectedEdges = [
+        {
+          id: 'OrderPlaced-0.0.1-LocationService-0.0.1',
+          source: 'OrderPlaced-0.0.1',
+          target: 'LocationService-0.0.1',
+          type: 'smoothstep',
+          label: 'receives event',
+          animated: false,
+          markerEnd: { type: 'arrow' },
+        },
+      ];
+
+      expect(nodes).toEqual(
+        expect.arrayContaining([
+          // Nodes on the left
+          expect.objectContaining(expectedEventNode),
+
+          // The command node itself
+          expect.objectContaining(expectedServiceNode),
+        ])
+      );
+
+      expect(edges).toEqual(expectedEdges);
+    });
+
+    it('should return nodes and edges for a given domain with services using semver range or latest version (version undefind)', async () => {
+      const { nodes, edges } = await getNodesAndEdges({ id: 'Checkout', version: '0.0.1' });
+
+      const expectedNodes = [
+        {
+          id: 'PlaceOrder-1.7.7',
+          sourcePosition: 'right',
+          targetPosition: 'left',
+          data: { mode: 'simple', message: mockCommands[2], showTarget: true },
+          position: { x: expect.any(Number), y: expect.any(Number) },
+          type: 'commands',
+        },
+        {
+          id: 'OrderService-1.0.0',
+          sourcePosition: 'right',
+          targetPosition: 'left',
+          data: {
+            mode: 'simple',
+            service: mockServices[2],
+            showSource: true,
+            showTarget: true,
+          },
+          position: { x: expect.any(Number), y: expect.any(Number) },
+          type: 'services',
+        },
+        {
+          id: 'OrderPlaced-0.0.1',
+          sourcePosition: 'right',
+          targetPosition: 'left',
+          data: { mode: 'simple', message: mockEvents[0], showSource: true, showTarget: true },
+          position: { x: expect.any(Number), y: expect.any(Number) },
+          type: 'events',
+        },
+        /** PAYMENT SERVICE */
+        {
+          id: 'PaymentService-0.0.1',
+          sourcePosition: 'right',
+          targetPosition: 'left',
+          data: {
+            mode: 'simple',
+            service: mockServices[3],
+            showSource: true,
+            showTarget: true,
+          },
+          position: { x: expect.any(Number), y: expect.any(Number) },
+          type: 'services',
+        },
+        {
+          id: 'PaymentPaid-0.0.1',
+          sourcePosition: 'right',
+          targetPosition: 'left',
+          data: { mode: 'simple', message: mockEvents[1], showSource: true },
+          position: { x: expect.any(Number), y: expect.any(Number) },
+          type: 'events',
+        },
+        {
+          id: 'PaymentPaid-0.0.2',
+          sourcePosition: 'right',
+          targetPosition: 'left',
+          data: { mode: 'simple', message: mockEvents[2], showSource: true },
+          position: { x: expect.any(Number), y: expect.any(Number) },
+          type: 'events',
+        },
+        {
+          id: 'PaymentRefunded-1.0.0',
+          sourcePosition: 'right',
+          targetPosition: 'left',
+          data: { mode: 'simple', message: mockEvents[4], showSource: true },
+          position: { x: expect.any(Number), y: expect.any(Number) },
+          type: 'events',
+        },
+        {
+          id: 'PaymentFailed-1.0.0',
+          sourcePosition: 'right',
+          targetPosition: 'left',
+          data: { mode: 'simple', message: mockEvents[6], showSource: true },
+          position: { x: expect.any(Number), y: expect.any(Number) },
+          type: 'events',
+        },
+      ];
+
+      const expectedEdges = [
+        {
+          id: 'PlaceOrder-1.7.7-OrderService-1.0.0',
+          source: 'PlaceOrder-1.7.7',
+          target: 'OrderService-1.0.0',
+          type: 'smoothstep',
+          label: 'accepts',
+          animated: false,
+          markerEnd: { type: 'arrow' },
+        },
+        {
+          id: 'OrderService-1.0.0-OrderPlaced-0.0.1',
+          source: 'OrderService-1.0.0',
+          target: 'OrderPlaced-0.0.1',
+          type: 'smoothstep',
+          label: 'publishes event',
+          animated: false,
+          markerEnd: { type: 'arrow' },
+        },
+        {
+          id: 'OrderPlaced-0.0.1-PaymentService-0.0.1',
+          source: 'OrderPlaced-0.0.1',
+          target: 'PaymentService-0.0.1',
+          type: 'smoothstep',
+          label: 'receives event',
+          animated: false,
+          markerEnd: { type: 'arrow' },
+        },
+        {
+          id: 'PaymentService-0.0.1-PaymentRefunded-1.0.0',
+          source: 'PaymentService-0.0.1',
+          target: 'PaymentRefunded-1.0.0',
+          type: 'smoothstep',
+          label: 'publishes event',
+          animated: false,
+          markerEnd: { type: 'arrow' },
+        },
+        {
+          id: 'PaymentService-0.0.1-PaymentFailed-1.0.0',
+          source: 'PaymentService-0.0.1',
+          target: 'PaymentFailed-1.0.0',
+          type: 'smoothstep',
+          label: 'publishes event',
+          animated: false,
+          markerEnd: { type: 'arrow' },
+        },
+        {
+          id: 'PaymentService-0.0.1-PaymentPaid-0.0.1',
+          source: 'PaymentService-0.0.1',
+          target: 'PaymentPaid-0.0.1',
+          type: 'smoothstep',
+          label: 'publishes event',
+          animated: false,
+          markerEnd: { type: 'arrow' },
+        },
+        {
+          id: 'PaymentService-0.0.1-PaymentPaid-0.0.2',
+          source: 'PaymentService-0.0.1',
+          target: 'PaymentPaid-0.0.2',
+          type: 'smoothstep',
+          label: 'publishes event',
+          animated: false,
+          markerEnd: { type: 'arrow' },
+        },
+      ];
+
+      expect(nodes).toStrictEqual(expect.arrayContaining(expectedNodes.map((n) => expect.objectContaining(n))));
+
+      expect(edges).toStrictEqual(expect.arrayContaining(expectedEdges));
+    });
+  });
+});
--- src/utils/domains/node-graph.ts
+++ src/utils/domains/node-graph.ts
@@ -1,7 +1,9 @@
 // import { getColor } from '@utils/colors';
-import { getCollection } from 'astro:content';
+import { getCollection, type CollectionEntry } from 'astro:content';
 import dagre from 'dagre';
 import { getNodesAndEdges as getServicesNodeAndEdges } from '../services/node-graph';
+import { validRange, satisfies } from 'semver';
+import merge from 'lodash.merge';
 
 type DagreGraph = any;
 
@@ -20,10 +22,32 @@ interface Props {
   mode?: 'simple' | 'full';
 }
 
+/**
+ * Get the services from the collection with the same id that satisfies the
+ * semver range (if version is defind) or the latest version (if version is not defined).
+ */
+const getVersion = (collection: CollectionEntry<'services'>[], id: string, version?: string): CollectionEntry<'services'>[] => {
+  const semverRange = validRange(version);
+
+  const filteredCollection = collection.filter((c) => c.data.id == id);
+
+  if (semverRange) {
+    return filteredCollection.filter((c) => satisfies(c.data.version, semverRange));
+  }
+
+  // Order by version
+  const sorted = filteredCollection.sort((a, b) => {
+    return a.data.version.localeCompare(b.data.version);
+  });
+
+  // latest version
+  return sorted.length > 0 ? [sorted[sorted.length - 1]] : [];
+};
+
 export const getNodesAndEdges = async ({ id, version, defaultFlow, mode = 'simple' }: Props) => {
   const flow = defaultFlow || getDagreGraph();
-  let nodes = [] as any,
-    edges = [] as any;
+  let nodes = new Map(),
+    edges = new Map();
 
   const domains = await getCollection('domains');
 
@@ -39,9 +63,16 @@ export const getNodesAndEdges = async ({ id, version, defaultFlow, mode = 'simpl
 
   const rawServices = domain?.data.services || [];
 
+  const servicesCollection = await getCollection('services');
+
+  const domainServicesWithVersion = rawServices
+    .map((service) => getVersion(servicesCollection, service.id, service.version))
+    .flat()
+    .map((svc) => ({ id: svc.data.id, version: svc.data.version }));
+
   // Get all the nodes for everyhing
 
-  for (const service of rawServices) {
+  for (const service of domainServicesWithVersion) {
     const { nodes: serviceNodes, edges: serviceEdges } = await getServicesNodeAndEdges({
       id: service.id,
       version: service.version,
@@ -49,12 +80,23 @@ export const getNodesAndEdges = async ({ id, version, defaultFlow, mode = 'simpl
       mode,
       renderAllEdges: true,
     });
-    nodes = [...nodes, ...serviceNodes];
-    edges = [...edges, ...serviceEdges];
+    serviceNodes.forEach((n) => {
+      /**
+       * A message could be sent by one service and received by another service on the same domain.
+       * So, we need deep merge the message to keep the `showSource` and `showTarget` as true.
+       *
+       * Let's see an example:
+       *  Take an `OrderPlaced` event sent by the `OrderService` `{ showSource: true }` and
+       *  received by `PaymentService` `{ showTarget: true }`.
+       */
+      nodes.set(n.id, nodes.has(n.id) ? merge(nodes.get(n.id), n) : n);
+    });
+    // @ts-ignore
+    serviceEdges.forEach((e) => edges.set(e.id, e));
   }
 
   return {
-    nodes: nodes,
-    edges: edges,
+    nodes: [...nodes.values()],
+    edges: [...edges.values()],
   };
 };
