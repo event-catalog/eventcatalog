acf4feeb8d6a3a49bf7a265c88839b6f85049090
carlosallexandre <carlos_costarodrigues@outlook.com>
2024-08-16 16:45:24 -0300

    test: add getServices test

---
+++ src/utils/__tests__/services/mocks.ts
@@ -0,0 +1,173 @@
+export const mockServices = [
+  {
+    id: 'services/Order/OrderService/index.mdx',
+    slug: 'services/Order/OrderService',
+    collection: 'services',
+    data: {
+      id: 'OrderService',
+      version: '1.0.0',
+      sends: [
+        {
+          id: 'OrderCreatedEvent',
+          version: '0.0.1',
+        },
+      ],
+      receives: [
+        {
+          id: 'PaymentProcessed',
+          version: '0.0.1',
+        },
+      ],
+    },
+  },
+  {
+    id: 'services/Inventory/InventoryService/index.mdx',
+    slug: 'services/Inventory/InventoryService',
+    collection: 'services',
+    data: {
+      id: 'InventoryService',
+      version: '1.0.0',
+      receives: [
+        {
+          id: 'OrderCreatedEvent',
+          version: '^1.3.0',
+        },
+      ],
+      sends: [
+        {
+          id: 'InventoryAdjusted',
+          version: '~2',
+        },
+      ],
+    },
+  },
+  {
+    id: 'services/Payment/PaymentService/index.mdx',
+    slug: 'services/Payment/PaymentService',
+    collection: 'services',
+    data: {
+      id: 'PaymentService',
+      version: '1.0.0',
+      receives: [
+        {
+          id: 'OrderCreatedEvent',
+        },
+      ],
+      sends: [
+        {
+          id: 'PaymentPaid',
+        },
+        {
+          id: 'PaymentFailed',
+          version: 'latest',
+        },
+      ],
+    },
+  },
+];
+
+export const mockEvents = [
+  {
+    slug: 'OrderCreatedEvent',
+    collection: 'events',
+    data: {
+      id: 'OrderCreatedEvent',
+      version: '0.0.1',
+    },
+  },
+  {
+    slug: 'OrderCreatedEvent',
+    collection: 'events',
+    data: {
+      id: 'OrderCreatedEvent',
+      version: '1.0.0',
+    },
+  },
+  {
+    slug: 'OrderCreatedEvent',
+    collection: 'events',
+    data: {
+      id: 'OrderCreatedEvent',
+      version: '1.3.9',
+    },
+  },
+  {
+    slug: 'OrderCreatedEvent',
+    collection: 'events',
+    data: {
+      id: 'OrderCreatedEvent',
+      version: '2.0.0',
+    },
+  },
+  {
+    slug: 'InventoryAdjusted',
+    collection: 'events',
+    data: {
+      id: 'InventoryAdjusted',
+      version: '0.0.1',
+    },
+  },
+  {
+    slug: 'InventoryAdjusted',
+    collection: 'events',
+    data: {
+      id: 'InventoryAdjusted',
+      version: '1.0.0',
+    },
+  },
+  {
+    slug: 'InventoryAdjusted',
+    collection: 'events',
+    data: {
+      id: 'InventoryAdjusted',
+      version: '2.0.0',
+    },
+  },
+  // 7
+  {
+    slug: 'PaymentPaid',
+    collection: 'events',
+    data: {
+      id: 'PaymentPaid',
+      version: '1.0.0',
+    },
+  },
+  // 8
+  {
+    slug: 'PaymentPaid',
+    collection: 'events',
+    data: {
+      id: 'PaymentPaid',
+      version: '2.0.0',
+    },
+  },
+  // 9
+  {
+    slug: 'PaymentFailed',
+    collection: 'events',
+    data: {
+      id: 'PaymentFailed',
+      version: '1.0.0',
+    },
+  },
+  // 10
+  {
+    slug: 'PaymentFailed',
+    collection: 'events',
+    data: {
+      id: 'PaymentFailed',
+      version: '1.2.3',
+    },
+  },
+];
+
+export const mockCommands = [
+  {
+    slug: 'PaymentProcessed',
+    collection: 'commands',
+    data: {
+      id: 'PaymentProcessed',
+      version: '0.0.1',
+    },
+  },
+];
---
+++ src/utils/__tests__/services/services.spec.ts
@@ -0,0 +1,113 @@
+import type { ContentCollectionKey } from 'astro:content';
+import { expect, describe, it, vi } from 'vitest';
+import { mockCommands, mockEvents, mockServices } from './mocks';
+import { getServices } from '@utils/services/services';
+
+vi.mock('astro:content', async (importOriginal) => {
+  return {
+    ...(await importOriginal<typeof import('astro:content')>()),
+    // this will only affect "foo" outside of the original module
+    getCollection: (key: ContentCollectionKey) => {
+      switch (key) {
+        case 'services':
+          return Promise.resolve(mockServices);
+        case 'events':
+          return Promise.resolve(mockEvents);
+        case 'commands':
+          return Promise.resolve(mockCommands);
+      }
+    },
+  };
+});
+
+describe('Services', () => {
+  describe('getServices', () => {
+    it('should returns an array of services', async () => {
+      const services = await getServices();
+
+      expect(services).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            collection: 'services',
+            data: expect.objectContaining({
+              id: 'OrderService',
+              version: '1.0.0',
+              receives: expect.any(Array),
+              sends: expect.any(Array),
+            }),
+          }),
+          expect.objectContaining({
+            collection: 'services',
+            data: expect.objectContaining({
+              id: 'InventoryService',
+              version: '1.0.0',
+              receives: expect.any(Array),
+              sends: expect.any(Array),
+            }),
+          }),
+        ])
+      );
+    });
+
+    it('should returns an array of services with messages (sends/receives)', async () => {
+      const services = await getServices();
+
+      expect(services).toEqual(
+        expect.arrayContaining([
+          /** OrderService */
+          expect.objectContaining({
+            id: 'services/Order/OrderService/index.mdx',
+            slug: 'services/Order/OrderService',
+            collection: 'services',
+            data: expect.objectContaining({
+              id: 'OrderService',
+              version: '1.0.0',
+              receives: [mockCommands[0]],
+              sends: [mockEvents[0]],
+            }),
+          }),
+        ])
+      );
+    });
+
+    it('should returns an array of services with messages (sends/receives) when version is specified as semver range', async () => {
+      const services = await getServices();
+
+      expect(services).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            id: 'services/Inventory/InventoryService/index.mdx',
+            slug: 'services/Inventory/InventoryService',
+            collection: 'services',
+            data: expect.objectContaining({
+              id: 'InventoryService',
+              version: '1.0.0',
+              receives: [mockEvents[2]],
+              sends: [mockEvents[6]],
+            }),
+          }),
+        ])
+      );
+    });
+
+    it('should returns an array of services with messages (sends/receives) when version is specified as latest or undefined', async () => {
+      const services = await getServices();
+
+      expect(services).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            id: 'services/Payment/PaymentService/index.mdx',
+            slug: 'services/Payment/PaymentService',
+            collection: 'services',
+            data: expect.objectContaining({
+              id: 'PaymentService',
+              version: '1.0.0',
+              receives: [mockEvents[3]],
+              sends: [mockEvents[8], mockEvents[10]],
+            }),
+          }),
+        ])
+      );
+    });
+  });
+});
