76bebd7bd0db03af4c623cdd8f1b58ccd53d1a35
omid eidivandi <eidivandi@live.com>
2024-09-05 16:47:21 +0200

    feat([AsyncApi]): add asyncapi viewer (#747)
    * feat([AsyncApi]): add asyncapi viewer
    
    * removed the MDX components in favour for new frontmatter API
    
    * Create moody-paws-whisper.md
    
    ---------
    
    Co-authored-by: glc-omid <oeidivandi@lacentrale.fr>
    Co-authored-by: David Boyne <davidboyne@Davids-MBP.upp.com>
    Co-authored-by: David Boyne <boyneyy123@gmail.com>

--- src/components/MDX/components.tsx
+++ src/components/MDX/components.tsx
@@ -5,7 +5,8 @@ import Accordion from '@components/MDX/Accordion/Accordion.astro';
 import AccordionGroup from '@components/MDX/Accordion/AccordionGroup.astro';
 import Flow from '@components/MDX/Flow/Flow.astro';
 import Admonition from '@components/MDX/Admonition';
-import OpenAPI from '@components/MDX/OpenAPI/OpenAPI';
+import OpenAPI from '@components/MDX/OpenAPI/OpenAPI.astro';
+import AsyncAPI from '@components/MDX/AsyncAPI/AsyncAPI.astro';
 
 //  Portals: required for server/client components
 import NodeGraphPortal from '@components/MDX/NodeGraph/NodeGraphPortal';
@@ -16,11 +17,12 @@ const components = (props: any) => {
     Accordion,
     AccordionGroup,
     Flow,
+    OpenAPI,
+    AsyncAPI,
     Admonition: (mdxProp: any) => <Admonition {...mdxProp} {...props} />,
     File: (mdxProp: any) => File({ ...props, ...mdxProp }),
     NodeGraph: (mdxProp: any) => NodeGraphPortal({ ...props.data, ...mdxProp }),
     SchemaViewer: (mdxProp: any) => SchemaViewerPortal({ ...props.data, ...mdxProp }),
-    OpenAPI: (mdxProp: any) => <OpenAPI {...mdxProp} {...props} />,
     Schema: (mdxProp: any) => Schema({ ...props, ...mdxProp }),
   };
 };
