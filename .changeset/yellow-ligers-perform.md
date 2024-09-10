d35846ba6d3e5d5ff12c9ac0304f5c3940e08ee5
David Boyne <boyneyy123@gmail.com>
2024-09-10 06:33:08 +0100

    chore(core): refactored the way pages are rendered reducing code (#761)
    * chore(core): refactored the way pages are rendered reducing code
    
    * Create yellow-ligers-perform.md
    
    * chore(core): refactored the way pages are rendered reducing code
    
    * chore(core): refactored the way pages are rendered reducing code
    
    * chore(core): refactored the way pages are rendered reducing code
    
    * chore(core): refactored the way pages are rendered reducing code
    
    ---------
    
    Co-authored-by: David Boyne <dboyne@Davids-MBP-2.upp.com>

---
+++ .changeset/yellow-ligers-perform.md
@@ -0,0 +1,5 @@
+---
+"@eventcatalog/core": patch
+---
+
+chore(core): refactored the way pages are rendered reducing code
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,4 @@
 export type CollectionTypes = 'commands' | 'events' | 'domains' | 'services' | 'flows';
 export type CollectionMessageTypes = 'commands' | 'events';
+
+export type PageTypes = 'events' | 'commands' | 'services' | 'domains';
