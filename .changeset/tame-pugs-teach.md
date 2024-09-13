f699aed1678a32d4fa87ee83f7576190dd51c6d9
David Boyne <boyneyy123@gmail.com>
2024-09-13 08:24:12 +0100

    feat(core): added utils to read and write catalog files (#788)
    * feat(core): added utils to read and write catalog files
    
    * feat(core): added utils to read and write catalog files
    
    * Create tame-pugs-teach.md
    
    ---------
    
    Co-authored-by: David Boyne <dboyne@Davids-MacBook-Pro-2.local>

---
+++ .changeset/tame-pugs-teach.md
@@ -0,0 +1,5 @@
+---
+"@eventcatalog/core": patch
+---
+
+feat(core): added utils to read and write catalog files
---
+++ scripts/eventcatalog-config-file-utils.js
@@ -0,0 +1,96 @@
+import { readFile, writeFile, rm } from 'node:fs/promises';
+import { existsSync } from 'fs';
+import path from 'node:path';
+
+// * Very strange behaviour when importing ESM files from catalogs into core.
+//  * Core (node) does not know how to handle ESM files, so we have to try and convert them.
+//  *
+//  * This needs sorting out! Sorry if you are reading this, but it unblocked me for now!
+//  * @param {*} content
+//  * @returns
+//  */
+function convertESMtoCJS(content) {
+  // Replace import statements with require
+  content = content.replace(/import\s+([a-zA-Z0-9{},\s*]+)\s+from\s+['"]([^'"]+)['"];/g, (match, imports, modulePath) => {
+    return `const ${imports.trim()} = require('${modulePath}');`;
+  });
+
+  // Replace export default with module.exports
+  content = content.replace(/export\s+default\s+/g, 'module.exports = ');
+
+  // Replace named exports with module.exports
+  content = content.replace(/export\s+{([^}]+)}/g, (match, exports) => {
+    return `module.exports = {${exports.trim()}};`;
+  });
+
+  // Remove declarations of __filename and __dirname
+  content = content.replace(/^\s*(const|let|var)\s+__(filename|dirname)\s+=\s+.*;?\s*$/gm, '');
+
+  return content;
+}
+
+export async function cleanup(projectDirectory) {
+  const filePath = path.join(projectDirectory, 'eventcatalog.config.cjs');
+  if (existsSync(filePath)) {
+    await rm(filePath);
+  }
+}
+
+export const getEventCatalogConfigFile = async (projectDirectory) => {
+  try {
+    const rawFile = await readFile(path.join(projectDirectory, 'eventcatalog.config.js'), 'utf8');
+
+    // Have to conver the ESM to CJS...
+    const configAsCommonJS = convertESMtoCJS(rawFile);
+
+    await writeFile(path.join(projectDirectory, 'eventcatalog.config.cjs'), configAsCommonJS);
+
+    const configAsCJS = await import(/* @vite-ignore */ path.join(projectDirectory, 'eventcatalog.config.cjs'));
+
+    //  Clean up?
+    await writeFile(path.join(projectDirectory, 'eventcatalog.config.js'), rawFile);
+
+    await cleanup(projectDirectory);
+
+    return configAsCJS.default;
+  } catch (error) {
+    await cleanup(projectDirectory);
+  }
+};
+
+export const writeEventCatalogConfigFile = async (projectDirectory, newConfig) => {
+  try {
+    const configFilePath = path.join(projectDirectory, 'eventcatalog.config.js');
+    let content = await readFile(configFilePath, 'utf8');
+
+    // Find the start of the config object
+    const startIndex = content.indexOf('export default {');
+    if (startIndex === -1) {
+      // Just fail silently if the config object is not found
+      return;
+    }
+
+    // Update or add each new config item
+    Object.entries(newConfig).forEach(([key, value]) => {
+      const valueString = JSON.stringify(value, null, 2).replace(/"/g, "'").replace(/\n/g, '\n  ');
+
+      // Check if the key already exists
+      const keyRegex = new RegExp(`(${key}\\s*:)([^,}]+)`, 'g');
+      if (content.match(keyRegex)) {
+        // Update existing key
+        content = content.replace(keyRegex, `$1 ${valueString}`);
+      } else {
+        // Add new key-value pair
+        const insertPosition = content.indexOf('{', startIndex) + 1;
+        content = content.slice(0, insertPosition) + `\n  ${key}: ${valueString},` + content.slice(insertPosition);
+      }
+    });
+
+    // Write the updated content back to the file
+    await writeFile(configFilePath, content);
+
+    await cleanup(projectDirectory);
+  } catch (error) {
+    await cleanup(projectDirectory);
+  }
+};
