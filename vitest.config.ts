d22373770afc9fcd472ba03ea45422853dce70e6
David Boyne <boyneyy123@gmail.com>
2024-10-16 18:09:05 +0100

    moving to exclude

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest" />
 import { getViteConfig } from 'astro/config';
+import { defaultExclude } from 'vitest/config';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default getViteConfig({
   plugins: [tsConfigPaths()],
   test: {
     globals: true,
-    include: ['**/__tests__/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    exclude: [...defaultExclude, 'e2e/**'],
   },
 });
