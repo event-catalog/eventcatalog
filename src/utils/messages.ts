8c6c1557146ab5bde03b4fcc43688576cda14af1
glc-omid <oeidivandi@lacentrale.fr>
2024-10-25 14:13:11 +0200

    fix([hp]): fix hp counters with only latest versions

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -2,14 +2,28 @@
 import { getCommands } from '@utils/commands';
 import { getEvents } from '@utils/events';
 import { getQueries } from './queries';
+import type { CollectionEntry } from 'astro:content';
 export { getCommands } from '@utils/commands';
 export { getEvents } from '@utils/events';
 
-// Main function that uses the imported functions
-export const getMessages = async () => {
-  const commands = await getCommands();
-  const events = await getEvents();
-  const queries = await getQueries();
+interface Props {
+  getAllVersions?: boolean;
+}
 
-  return { commands, events, queries };
+type Messages = {
+  commands: CollectionEntry<'commands'>[];
+  events: CollectionEntry<'events'>[];
+  queries: CollectionEntry<'queries'>[];
+};
+// Main function that uses the imported functions
+export const getMessages = async ({ getAllVersions = true }: Props = {}): Promise<Messages> => {
+  const commands = await getCommands({ getAllVersions });
+  const events = await getEvents({ getAllVersions });
+  const queries = await getQueries({ getAllVersions });
+
+  return {
+    commands: commands as CollectionEntry<'commands'>[],
+    events: events as CollectionEntry<'events'>[],
+    queries: queries as CollectionEntry<'queries'>[],
+  } satisfies Messages;
 };
