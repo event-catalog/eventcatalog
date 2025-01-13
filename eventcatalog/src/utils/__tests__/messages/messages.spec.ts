1764ef900eccb309a8b4f1f4f417474cb0cbd2f9
carlosallexandre <carlos_costarodrigues@outlook.com>
2025-01-13 17:21:16 -0300

    fix: remove cache from `getMessages` function
    The downstream functions getEvents, getCommands, and getQueries
    handle the cache appropriately. The getMessages function doesn't
    differentiate the cache between currentVersions and allVersions
    as the others do.

---
+++ eventcatalog/src/utils/__tests__/messages/messages.spec.ts
@@ -0,0 +1,142 @@
+import type { CollectionEntry, ContentCollectionKey } from 'astro:content';
+import { expect, describe, it, vi } from 'vitest';
+import { mockCommands, mockEvents, mockQueries, mockServices, mockChannels } from './mocks';
+import { getMessages } from '@utils/messages';
+
+vi.mock('astro:content', async (importOriginal) => {
+  return {
+    ...(await importOriginal<typeof import('astro:content')>()),
+    async getCollection<T extends ContentCollectionKey>(key: T, filterFn: (entry: CollectionEntry<T>) => boolean = () => true) {
+      switch (key) {
+        case 'services':
+          return mockServices.filter(filterFn);
+        case 'events':
+          return mockEvents.filter(filterFn as any);
+        case 'commands':
+          return mockCommands.filter(filterFn as any);
+        case 'channels':
+          return mockChannels.filter(filterFn);
+        case 'queries':
+          return mockQueries.filter(filterFn);
+      }
+    },
+  };
+});
+
+describe('getMessages', () => {
+  it('should return all versions of messages', async () => {
+    await expect(getMessages({ getAllVersions: true })).resolves.toEqual(
+      expect.objectContaining({
+        events: expect.arrayContaining([
+          expect.objectContaining({
+            collection: 'events',
+            data: expect.objectContaining({
+              id: 'PaymentProcessed',
+              version: '0.0.1',
+            }),
+          }),
+          expect.objectContaining({
+            collection: 'events',
+            data: expect.objectContaining({
+              id: 'PaymentProcessed',
+              version: '0.0.2',
+            }),
+          }),
+          expect.objectContaining({
+            collection: 'events',
+            data: expect.objectContaining({
+              id: 'PaymentProcessed',
+              version: '0.1.0',
+            }),
+          }),
+        ]),
+      })
+    );
+  });
+
+  it('should return current versions of messages', async () => {
+    await expect(getMessages({ getAllVersions: false })).resolves.toEqual({
+      queries: [],
+      commands: [
+        expect.objectContaining({
+          collection: 'commands',
+          data: expect.objectContaining({
+            id: 'ProcessPayment',
+            version: '0.1.0',
+          }),
+        }),
+      ],
+      events: [
+        expect.objectContaining({
+          collection: 'events',
+          data: expect.objectContaining({
+            id: 'PaymentProcessed',
+            version: '0.1.0',
+          }),
+        }),
+      ],
+    });
+  });
+
+  it('should return current versions even if the first call is to get all versions', async () => {
+    await getMessages({ getAllVersions: true });
+
+    const messages = await getMessages({ getAllVersions: false });
+
+    expect(messages).toEqual({
+      queries: [],
+      commands: [
+        expect.objectContaining({
+          collection: 'commands',
+          data: expect.objectContaining({
+            id: 'ProcessPayment',
+            version: '0.1.0',
+          }),
+        }),
+      ],
+      events: [
+        expect.objectContaining({
+          collection: 'events',
+          data: expect.objectContaining({
+            id: 'PaymentProcessed',
+            version: '0.1.0',
+          }),
+        }),
+      ],
+    });
+  });
+
+  it('should return all version even if the first call is to get current versions', async () => {
+    await getMessages({ getAllVersions: false });
+
+    const messages = await getMessages({ getAllVersions: true });
+
+    expect(messages).toEqual(
+      expect.objectContaining({
+        commands: expect.arrayContaining([
+          expect.objectContaining({
+            collection: 'commands',
+            data: expect.objectContaining({
+              id: 'ProcessPayment',
+              version: '0.0.1',
+            }),
+          }),
+          expect.objectContaining({
+            collection: 'commands',
+            data: expect.objectContaining({
+              id: 'ProcessPayment',
+              version: '0.0.2',
+            }),
+          }),
+          expect.objectContaining({
+            collection: 'commands',
+            data: expect.objectContaining({
+              id: 'ProcessPayment',
+              version: '0.1.0',
+            }),
+          }),
+        ]),
+      })
+    );
+  });
+});
---
+++ eventcatalog/src/utils/__tests__/messages/mocks.ts
@@ -0,0 +1,65 @@
+export const mockCommands = [
+  {
+    id: 'ProcessPayment',
+    collection: 'commands',
+    data: {
+      id: 'ProcessPayment',
+      version: '0.0.1',
+      pathToFile: 'commands/ProcessPayment/versioned/0.0.1/index.md',
+    },
+  },
+  {
+    id: 'ProcessPayment',
+    collection: 'commands',
+    data: {
+      id: 'ProcessPayment',
+      version: '0.0.2',
+      pathToFile: 'commands/ProcessPayment/versioned/0.0.2/index.md',
+    },
+  },
+  {
+    id: 'ProcessPayment',
+    collection: 'commands',
+    data: {
+      id: 'ProcessPayment',
+      version: '0.1.0',
+      pathToFile: 'commands/ProcessPayment/index.md',
+    },
+  },
+];
+
+export const mockEvents = [
+  {
+    id: 'PaymentProcessed',
+    collection: 'events',
+    data: {
+      id: 'PaymentProcessed',
+      version: '0.0.1',
+      pathToFile: 'events/PaymentProcessed/versioned/0.0.1/index.md',
+    },
+  },
+  {
+    id: 'PaymentProcessed',
+    collection: 'events',
+    data: {
+      id: 'PaymentProcessed',
+      version: '0.0.2',
+      pathToFile: 'events/PaymentProcessed/versioned/0.0.2/index.md',
+    },
+  },
+  {
+    id: 'PaymentProcessed',
+    collection: 'events',
+    data: {
+      id: 'PaymentProcessed',
+      version: '0.1.0',
+      pathToFile: 'events/PaymentProcessed/index.md',
+    },
+  },
+];
+
+export const mockQueries = [];
+
+export const mockServices = [];
+
+export const mockChannels = [];
