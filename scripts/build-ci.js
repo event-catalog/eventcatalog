44d3fcf461ff98498ccdd04029da694d54f0c869
David Boyne <boyneyy123@gmail.com>
2025-02-10 17:21:01 +0000

    update

--- scripts/build-ci.js
+++ scripts/build-ci.js
@@ -21,5 +21,6 @@ execSync(`cross-env NODE_ENV=CI PROJECT_DIR=${projectDIR} CATALOG_DIR=${catalogD
 
 // Type check
 execSync(`pnpm exec astro check --minimumSeverity error`, {
+  cwd: catalogDir,
   stdio: 'inherit',
 });
