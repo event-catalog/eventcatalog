9418f2883c926e2748ed6f3d6c4b1a497a3114a9
David Boyne <boyneyy123@gmail.com>
2025-02-12 12:57:01 +0000

    chore(core): added backstage license checks to embed eventcatalog

---
+++ src/features.ts
@@ -0,0 +1,62 @@
+import boxen from 'boxen';
+
+type LicenseResponse = {
+  is_trial: boolean;
+  plugin: string;
+  state: string;
+};
+
+// Checks to see if the backstage feature is enabled (or not)
+export const isBackstagePluginEnabled = async (licenseKey?: string) => {
+  const LICENSE_KEY = process.env.EVENTCATALOG_LICENSE_KEY_BACKSTAGE || null;
+
+  // no license key, so it's not enabled
+  if (!LICENSE_KEY) {
+    return false;
+  }
+
+  // Verify the license key
+  const response = await fetch('https://api.eventcatalog.cloud/functions/v1/license', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${LICENSE_KEY}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.status !== 200) {
+    console.log(
+      '\nTried to verify your backstage license but it is not valid. Please check your license key or purchase a license at https://eventcatalog.cloud/\n'
+    );
+    return false;
+  }
+
+  if (response.status === 200) {
+    const data = (await response.json()) as LicenseResponse;
+
+    if ('@eventcatalog/backstage-plugin-eventcatalog' !== data.plugin) {
+      console.log(
+        '\nInvalid license key for backstage integration, please check your license key or purchase a license at https://eventcatalog.cloud/\n'
+      );
+      return false;
+    }
+
+    let message = 'Backstage integration is enabled for EventCatalog';
+
+    if (data.is_trial) {
+      message += '\nYou are using a trial license for backstage integration.';
+    }
+
+    console.log(
+      boxen(message, {
+        padding: 1,
+        margin: 1,
+        borderColor: 'green',
+        title: '@eventcatalog/backstage-plugin-eventcatalog',
+        titleAlignment: 'center',
+      })
+    );
+  }
+
+  return true;
+};
