588fd6228d90ff38f2b6f697189f87b867e668a7
David Boyne <boyneyy123@gmail.com>
2025-02-13 12:27:24 +0000

    chore(core): small ui improvements

--- eventcatalog/tailwind.config.mjs
+++ eventcatalog/tailwind.config.mjs
@@ -50,5 +50,5 @@ export default {
     'border-l-blue-500',
   ],
 
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'), require('tailwind-scrollbar')],
 };
