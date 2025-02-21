bf92c1d3bd6de10b05f19b7004f2107fda93d728
Carlos Rodrigues <carlos_costarodrigues@outlook.com>
2025-02-21 12:01:57 -0300

    fix/windows-path-issue-tree-view (#1179)
    * fix(core): ensure glob paths work correctly on Windows
    
    This sets the `windowsPathsNoEscape: true` on `globSync` when running
    on Windows(`os.platform() === 'win32'`) to avoid path escaping issues.
    
    * Create nervous-students-happen.md
    
    ---------
    
    Co-authored-by: David Boyne <boyneyy123@gmail.com>

--- eventcatalog/src/components/SideNav/TreeView/getTreeView.ts
+++ eventcatalog/src/components/SideNav/TreeView/getTreeView.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import os from 'node:os';
 import gm from 'gray-matter';
 import { globSync } from 'glob';
 import type { CollectionKey } from 'astro:content';
@@ -49,7 +50,7 @@ function buildTreeOfDir(directory: string, parentNode: TreeNode, options: { igno
 
   const resourceType = getResourceType(directory);
 
-  const markdownFiles = globSync(path.join(directory, '/*.md'));
+  const markdownFiles = globSync(path.join(directory, '/*.md'), { windowsPathsNoEscape: os.platform() === 'win32' });
   const isResourceIgnored = options?.ignore && resourceType && options.ignore.includes(resourceType);
 
   if (markdownFiles.length > 0 && !isResourceIgnored) {
