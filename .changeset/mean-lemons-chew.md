8526a174bd631a41111654075c5804e3d1f257c7
David Boyne <boyneyy123@gmail.com>
2025-04-07 18:54:59 +0100

    feat(core): added experimental api for catalog (#1354)
    * feat(core): added experimental api for catalog
    
    * Create mean-lemons-chew.md
    
    * feat(core): added experimental api for catalog

---
+++ .changeset/mean-lemons-chew.md
@@ -0,0 +1,5 @@
+---
+"@eventcatalog/core": patch
+---
+
+feat(core): added experimental api for catalog
---
+++ eventcatalog/src/pages/api/catalog.ts
@@ -0,0 +1,26 @@
+import type { APIRoute } from 'astro';
+import utils from '@eventcatalog/sdk';
+import config from '@config';
+
+/**
+ * Route the will dump the whole catalog as JSON (without markdown)
+ * Experimental API
+ * @param param0
+ * @returns
+ */
+export const GET: APIRoute = async ({ params, request }) => {
+  if (!config.api?.fullCatalogAPIEnabled) {
+    return new Response(JSON.stringify({ error: 'Full catalog API is not enabled' }), {
+      status: 404,
+    });
+  }
+
+  const { dumpCatalog } = utils(process.env.PROJECT_DIR || '');
+  const catalog = await dumpCatalog({ includeMarkdown: false });
+
+  return new Response(JSON.stringify(catalog), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
