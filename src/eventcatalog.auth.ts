0dbdeae0f63787bf1afdcf652ed47e2b3a60408b
David Boyne <boyneyy123@gmail.com>
2025-06-12 11:58:42 +0100

    feat(core): added auth0 to list of providers (#1521)
    * feat(core): added auth0 to list of providers
    
    * Create spotty-socks-tan.md
    
    * feat(core): added auth0 to list of providers

--- src/eventcatalog.auth.ts
+++ src/eventcatalog.auth.ts
@@ -2,11 +2,13 @@
 import type { OAuthUserConfig } from '@auth/core/providers';
 import type { GitHubProfile } from '@auth/core/providers/github';
 import type { OktaProfile } from '@auth/core/providers/okta';
+import type { Auth0Profile } from '@auth/core/providers/auth0';
 
 export interface EventCatalogAuthConfig {
   providers?: {
     github?: OAuthUserConfig<GitHubProfile>;
     okta?: OAuthUserConfig<OktaProfile>;
+    auth0?: OAuthUserConfig<Auth0Profile>;
   };
   // Enterprise only features
   customAuthConfig?: string;
