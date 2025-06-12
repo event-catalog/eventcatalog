1bc6ee40b40d76efcff11a4e59f731815641af6a
David Boyne <boyneyy123@gmail.com>
2025-06-12 14:50:05 +0100

    feat(core): added entra to auth providers (#1523)
    * feat(core): added entra to auth providers
    
    * Create late-jokes-pump.md

--- src/eventcatalog.auth.ts
+++ src/eventcatalog.auth.ts
@@ -3,12 +3,14 @@ import type { OAuthUserConfig } from '@auth/core/providers';
 import type { GitHubProfile } from '@auth/core/providers/github';
 import type { OktaProfile } from '@auth/core/providers/okta';
 import type { Auth0Profile } from '@auth/core/providers/auth0';
+import type { MicrosoftEntraIDProfile } from '@auth/core/providers/microsoft-entra-id';
 
 export interface EventCatalogAuthConfig {
   providers?: {
     github?: OAuthUserConfig<GitHubProfile>;
     okta?: OAuthUserConfig<OktaProfile>;
     auth0?: OAuthUserConfig<Auth0Profile>;
+    entra?: OAuthUserConfig<MicrosoftEntraIDProfile>;
   };
   // Enterprise only features
   customAuthConfig?: string;
