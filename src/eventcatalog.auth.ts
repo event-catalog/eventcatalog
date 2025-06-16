17eef2c35c0e38c05757dc6d66b9d915aa66d955
David Boyne <boyneyy123@gmail.com>
2025-06-16 12:39:06 +0100

    feat(core): added google auth

--- src/eventcatalog.auth.ts
+++ src/eventcatalog.auth.ts
@@ -4,10 +4,12 @@ import type { GitHubProfile } from '@auth/core/providers/github';
 import type { OktaProfile } from '@auth/core/providers/okta';
 import type { Auth0Profile } from '@auth/core/providers/auth0';
 import type { MicrosoftEntraIDProfile } from '@auth/core/providers/microsoft-entra-id';
+import type { GoogleProfile } from '@auth/core/providers/google';
 
 export interface EventCatalogAuthConfig {
   providers?: {
     github?: OAuthUserConfig<GitHubProfile>;
+    google?: OAuthUserConfig<GoogleProfile>;
     okta?: OAuthUserConfig<OktaProfile>;
     auth0?: OAuthUserConfig<Auth0Profile>;
     entra?: OAuthUserConfig<MicrosoftEntraIDProfile>;
