b07a27a030bf89ab5ad1ed912be6f25c1bfc8ddf
David Boyne <boyneyy123@gmail.com>
2025-06-17 12:24:03 +0100

    feat(core): added auth middleware for rbac

--- examples/default/middleware.ts
+++ examples/default/middleware.ts
@@ -1,7 +1,4 @@
 import type { MiddlewareHandler } from 'astro';
-// @ts-ignore
-import { findMatchingRule } from '@middleware/auth';
-
 interface Locals {
   hasRole: (role: string) => boolean;
   hasGroup: (group: string) => boolean;
@@ -12,18 +9,20 @@ export const rbacMiddleware: MiddlewareHandler = async (context, next) => {
   const { locals, url } = context;
   const pathname = url.pathname;
 
-  const { hasRole, hasGroup } = locals as Locals;
+  const { hasRole, hasGroup, findMatchingRule } = locals as Locals;
 
   const accessRules = {
     '/docs/domains/E-Commerce/*': () => !hasGroup('Viewer'),
     '/visualiser/domains/E-Commerce/*': () => !hasGroup('Viewer'),
   };
 
-  // Use the utility from locals - no import needed!
-  const rule = findMatchingRule(accessRules, pathname);
-  
-  if (rule && !rule()) {
-    return new Response('Forbidden', { status: 403 });
+  if (findMatchingRule) {
+    // Use the utility from locals - no import needed!
+    const rule = findMatchingRule(accessRules, pathname);
+
+    if (rule && !rule()) {
+      return new Response('Forbidden', { status: 403 });
+    }
   }
 
   return next();
