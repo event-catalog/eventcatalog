1c9cb518751c9f10a9cfba08f0256c2c961fac8f
David Boyne <boyneyy123@gmail.com>
2025-06-20 16:28:32 +0100

    feat(core) added props to schema viewer

--- eventcatalog/src/components/MDX/SchemaViewer/SchemaViewerPortal.tsx
+++ eventcatalog/src/components/MDX/SchemaViewer/SchemaViewerPortal.tsx
@@ -1,7 +1,15 @@
 const SchemaViewerPortal = (props: any) => {
-  console.log('props', props);
+  // Convert string props to booleans (MDX passes strings)
+  const expandBool = props.expand === true || props.expand === 'true';
+  const searchBool = props.search !== false && props.search !== 'false';
+
   return (
-    <div id={`${props.id}-${props.file}-SchemaViewer-portal`} data-expand={props.expand} data-max-height={props.maxHeight} />
+    <div
+      id={`${props.id}-${props.file}-SchemaViewer-portal`}
+      data-expand={expandBool ? 'true' : 'false'}
+      data-max-height={props.maxHeight}
+      data-search={searchBool ? 'true' : 'false'}
+    />
   );
 };
 
