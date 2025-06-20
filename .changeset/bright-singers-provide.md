79ec9141bd32b2750b6544cec6b8e25f7a616a26
David Boyne <boyneyy123@gmail.com>
2025-06-20 16:50:42 +0100

    feat(core): added search and expand features to schema viewer (#1552)
    * feat(core) added props to schema viewer
    
    * feat(core) added props to schema viewer
    
    * feat(core) added props to schema viewer
    
    * Create bright-singers-provide.md

---
+++ .changeset/bright-singers-provide.md
@@ -0,0 +1,5 @@
+---
+"@eventcatalog/core": patch
+---
+
+feat(core): added search and expand features to schema viewer
--- eventcatalog/src/components/MDX/SchemaViewer/SchemaViewerPortal.tsx
+++ eventcatalog/src/components/MDX/SchemaViewer/SchemaViewerPortal.tsx
@@ -1,5 +1,16 @@
 const SchemaViewerPortal = (props: any) => {
-  return <div id={`${props.id}-${props.file}-SchemaViewer-portal`} />;
+  // Convert string props to booleans (MDX passes strings)
+  const expandBool = props.expand === true || props.expand === 'true';
+  const searchBool = props.search !== false && props.search !== 'false';
+
+  return (
+    <div
+      id={`${props.id}-${props.file}-SchemaViewer-portal`}
+      data-expand={expandBool ? 'true' : 'false'}
+      data-max-height={props.maxHeight}
+      data-search={searchBool ? 'true' : 'false'}
+    />
+  );
 };
 
 export default SchemaViewerPortal;
