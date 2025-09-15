ea39ffaa136d84790e2e5817ed70741af168145b
kbreton <kbreton@knowbe4.com>
2025-09-15 14:42:54 -0400

    Increase static page generation timeout

--- packages/eventcatalog/next.config.js
+++ packages/eventcatalog/next.config.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require('./eventcatalog.config');
 
-module.exports = {
+module.exports = {  
+  staticPageGenerationTimeout: parseInt(process.env.STATIC_PAGE_GENERATION_TIMEOUT || '60', 10), // Use an environment variable or use the default to 60 seconds.
   reactStrictMode: true,
   basePath: config.basePath,
   trailingSlash: config.trailingSlash,
