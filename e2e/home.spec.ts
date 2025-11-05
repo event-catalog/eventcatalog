c009458ff748c3c437404d7665ba98b0a8d42f93
David Boyne <boyneyy123@gmail.com>
2025-11-05 16:05:15 +0000

    chore(core): updated astro versions (#1806)
    * chore(core): updated astro versions
    
    * Create twenty-parents-guess.md
    
    * chore(core): updated astro versions

--- e2e/home.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('has title', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.getByRole('heading', { name: 'Welcome to EventCatalog' })).toBeVisible();
-});
