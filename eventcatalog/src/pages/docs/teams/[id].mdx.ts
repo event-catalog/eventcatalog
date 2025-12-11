182c143f8d0523f6ab95f5fce8812c45e219e9c5
David Boyne <boyneyy123@gmail.com>
2025-12-11 19:48:06 +0000

    fix(core): mdx pages are added to teams and users

---
+++ eventcatalog/src/pages/docs/teams/[id].mdx.ts
@@ -0,0 +1,36 @@
+// This file exposes the markdown for EventCatalog in the Url
+// For example http://localhost:3000/docs/teams/full-stack loads the Page and http://localhost:3000/docs/teams/full-stack.mdx loads the markdown
+// This is used for the LLMs to load the markdown for the given item (llms.txt);
+
+import type { APIRoute } from 'astro';
+import { getCollection } from 'astro:content';
+import config from '@config';
+import fs from 'fs';
+
+const teams = await getCollection('teams');
+
+export async function getStaticPaths() {
+  // Just return empty array if LLMs are not enabled
+  if (!config.llmsTxt?.enabled) {
+    return [];
+  }
+
+  return teams.map((team) => ({
+    params: { type: 'teams', id: team.data.id },
+    props: { content: team },
+  }));
+}
+
+export const GET: APIRoute = async ({ params, props }) => {
+  // Just return empty array if LLMs are not enabled
+  if (!config.llmsTxt?.enabled) {
+    return new Response('llms.txt is not enabled for this Catalog.', { status: 404 });
+  }
+
+  if (props?.content?.filePath) {
+    const file = fs.readFileSync(props.content.filePath, 'utf8');
+    return new Response(file, { status: 200 });
+  }
+
+  return new Response('Not found', { status: 404 });
+};
