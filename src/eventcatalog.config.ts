7d50a8eee727bb4ac3cc5011575e6bbc7e9a5e10
David Boyne <boyneyy123@gmail.com>
2025-12-11 19:54:07 +0000

    chore(core): auth is now more explict opt in

--- src/eventcatalog.config.ts
+++ src/eventcatalog.config.ts
@@ -51,6 +51,10 @@ type PagesConfiguration = {
   pages?: string[];
 };
 
+type AuthConfig = {
+  enabled: boolean;
+};
+
 export interface Config {
   title: string;
   tagline: false;
@@ -64,6 +68,7 @@ export interface Config {
   host?: string;
   trailingSlash?: boolean;
   output?: 'server' | 'static';
+  auth?: AuthConfig;
   rss?: {
     enabled: boolean;
     limit: number;
