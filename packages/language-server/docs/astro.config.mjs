7d5fbb39e2569f177f267b41f0cea6c0fb011e0f
David Boyne <boyneyy123@gmail.com>
2026-03-02 17:21:22 +0000

    update docs

--- packages/language-server/docs/astro.config.mjs
+++ packages/language-server/docs/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 export default defineConfig({
+  base: "/docs",
   integrations: [
     starlight({
       title: "EventCatalog Compass",
