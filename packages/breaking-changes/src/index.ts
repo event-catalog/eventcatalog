2e6acf82ac416b8f877eabb5558ca5e0d68121b2
David Boyne <boyneyy123@gmail.com>
2026-03-23 15:19:06 +0000

    feat(breaking-changes): scaffold package

---
+++ packages/breaking-changes/src/index.ts
@@ -0,0 +1,4 @@
+export type { CompatibilityStrategy, SchemaChange, BreakingChange, SchemaChangeType } from './types';
+
+export { detectBreakingChanges } from './json-schema/rules';
+export { diffJsonSchemas } from './json-schema/differ';
