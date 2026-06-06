1369f0f6847fb59f5120638adb88c4d19a463d88
David Boyne <boyneyy123@gmail.com>
2026-06-06 17:17:55 +0100

    update

--- packages/core/eventcatalog/src/components/MDX/SchemaViewer/SchemaViewerPortal.tsx
+++ packages/core/eventcatalog/src/components/MDX/SchemaViewer/SchemaViewerPortal.tsx
@@ -2,11 +2,10 @@ const SchemaViewerPortal = (props: any) => {
   // Convert string props to booleans (MDX passes strings)
   const expandBool = props.expand === true || props.expand === 'true';
   const searchBool = props.search !== false && props.search !== 'false';
-  const schemaKey = props.file || 'default';
 
   return (
     <div
-      id={`${props.id}-${schemaKey}-SchemaViewer-portal`}
+      data-schema-viewer-portal-id={props.id}
       data-expand={expandBool ? 'true' : 'false'}
       data-max-height={props.maxHeight}
       data-search={searchBool ? 'true' : 'false'}
